949bc9987471036ebe51b135df2fe1343398d2c9
Brian Zanti <brianzanti@gmail.com>
2020-12-08 16:00:32 -0700

    validates numericality of pet age

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,5 +2,9 @@ class Pet < ApplicationRecord
   belongs_to :shelter
   validates_presence_of :name, :description, :approximate_age, :sex
 
+  validates :approximate_age, numericality: {
+              greater_than_or_equal_to: 0
+            }
+
   enum sex: [:female, :male]
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -9,6 +9,8 @@ describe Pet, type: :model do
     it {should validate_presence_of :name}
     it {should validate_presence_of :description}
     it {should validate_presence_of :sex}
+    it {should validate_numericality_of(:approximate_age).is_greater_than_or_equal_to(0)}
+
     it 'is created as adoptable by default' do
       shelter = Shelter.create!(name: 'Pet Rescue', address: '123 Adoption Ln.', city: 'Denver', state: 'CO', zip: '80222')
       pet = shelter.pets.create!(name: "Fluffy", approximate_age: 3, sex: 'male', description: 'super cute')

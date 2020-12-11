b415bc2994ff557d4bc8f2dfc77e39aa34a92d38
Vanessa Arruda <wanessaarruda@gmail.com>
2020-12-11 13:44:55 -0700

    Install faker gem

--- spec/factories/application.rb
+++ spec/factories/application.rb
@@ -1,9 +1,9 @@
-# FactoryBot.define do
-#   factory :application do
-#     name { "#{Faker::Company.industry} #{["Conference", "Convention","Symposium"].sample}" }
-#     organization { Faker::Company.name }
-#     size { rand(50..10000) }
-#     start_date { Faker::Date.between(from: 1.year.ago, to: 1.year.from_now) }
-#     end_date { Faker::Date.between(from: start_date, to: start_date + 10) }
-#   end
-# end
+FactoryBot.define do
+  factory :application do
+    name { "#{Faker::Company.industry} #{["Conference", "Convention","Symposium"].sample}" }
+    organization { Faker::Company.name }
+    size { rand(50..10000) }
+    start_date { Faker::Date.between(from: 1.year.ago, to: 1.year.from_now) }
+    end_date { Faker::Date.between(from: start_date, to: start_date + 10) }
+  end
+end

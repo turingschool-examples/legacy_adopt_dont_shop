2a23627a6780c9c035f55b06a030c97e168683a8
Ely Hess <ely.hess@me.com>
2020-12-11 15:31:55 -0700

    Add resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :shelters
+  # resources :shelters
   # get "/shelters", to: "shelters#index"
   # get "/shelters/new", to: "shelters#new"
   # get "/shelters/:id", to: "shelters#show"
@@ -11,19 +11,20 @@ Rails.application.routes.draw do
   # post "/shelters", to: "shelters#create"
   # get "/shelters/:id/edit", to: "shelters#edit"
   # patch "/shelters/:id", to: "shelters#update"
-  resources :pets
+  
+  # resources :pets
   # get "/pets", to: "pets#index"
   # get "/pets/:id", to: "pets#show"
   # get "/pets/:id/edit", to: "pets#edit"
   # patch "/pets/:id", to: "pets#update"
   # delete "/pets/:id", to: "pets#destroy"
-
-  # resources :shelters do
-  #   resources :pets, only: [:index, :new]
-  # end
-
-  get "/shelters/:shelter_id/pets", to: "shelter_pets#index", as: :shelter_pets
-  get "/shelters/:shelter_id/pets/new", to: "shelter_pets#new", as: :new_shelter_pet
-  post "/shelters/:shelter_id/pets", to: "shelter_pets#create"
-  get "/shelters/:id/pets/:id", to: "pets#show", as: :shelter_pet
+  
+  resources :pets
+  resources :shelters do
+    resources :pets, controller: "shelter_pets", only: [:index, :new, :create]
+  end
+  # get "/shelters/:shelter_id/pets", to: "shelter_pets#index", as: :shelter_pets
+  # get "/shelters/:shelter_id/pets/new", to: "shelter_pets#new", as: :new_shelter_pet
+  # post "/shelters/:shelter_id/pets", to: "shelter_pets#create"
+  # get "/shelters/:shelter_id/pets/:id", to: "pets#show", as: :shelter_pet
 end

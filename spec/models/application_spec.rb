fee8d7391fb61fde9a1c1ee51d25a571ae3c5d63
awosborn <alanturing@example.coclear>
2020-12-11 18:03:49 -0700

    Try to fix migration

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe PetApplication, type: :model do
+RSpec.describe Application, type: :model do
   describe 'validations' do
     # it { should validate_presence_of :}
   end
 
   describe 'relationships' do
-    it {should belong_to :pet}
-    it {should belong_to :application}
+    it {should have_many :pet_applications}
   end
 
   describe 'instance methods' do

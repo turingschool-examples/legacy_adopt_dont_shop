29f6095cc620b54594475c103e45f401192c2f3c
Grayson Cunningham <gecun289@gmail.com>
2020-12-12 16:54:47 -0700

    add tests for application_pet status and approval

--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -5,4 +5,28 @@ RSpec.describe ApplicationPet do
     it { should belong_to :application}
     it { should belong_to :pet}
   end
+
+  describe 'instance methods' do
+    it 'does not have a status on creation' do
+      application_pet = create(:application_pet)
+
+      expect(application_pet.status).to be_nil
+    end
+
+    it 'can be approved' do
+      application_pet = create(:application_pet)
+
+      application_pet.approve
+
+      expect(application_pet.status).to eq("Approved")
+    end
+
+    it 'can be rejected' do
+      application_pet = create(:application_pet)
+
+      application_pet.approve
+
+      expect(application_pet.status).to eq("Approved")
+    end
+  end
 end

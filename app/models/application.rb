fa2bba474a52a1d974212f9261cfc027a435203f
Ely Hess <ely.hess@me.com>
2020-12-12 01:51:55 -0700

    Add application validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,11 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+
+  validates_presence_of :name
+  validates_presence_of :street
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip_code
+  validates :zip_code, zipcode: { country_code: :us }
 end

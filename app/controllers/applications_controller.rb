c9006a94a9b2334ba078e2f58d1725e35466ddaf
Ely Hess <ely.hess@me.com>
2020-12-12 14:53:35 -0700

    Add pet_search logic

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
+    if params[:pet_search]
+      @pets = Pet.all.search_pet_by_name(params[:pet_search])
+    end
   end
 
   def new
@@ -15,6 +18,11 @@ class ApplicationsController < ApplicationController
       flash[:failure] = "Application not created: Required information missing or invalid."
       render :new
     end
+  end
+
+  def adopt
+    @application = Application.find(params[:id])
+    
   end
 
   private

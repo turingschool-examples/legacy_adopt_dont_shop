3025ce85bcf7af44535a6c17dafa425cbc7ff07d
Adam Etzion <adametzion90@gmail.com>
2020-12-12 15:11:21 -0700

    Add validation to application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,8 @@ class Application < ApplicationRecord
     belongs_to :applicant
     has_many :application_pets
     has_many :pets, through: :application_pets
+
+    validates_presence_of :description
+
+    enum sex: [:In_Progress, :Pending, :Accepted, :Rejected]
 end

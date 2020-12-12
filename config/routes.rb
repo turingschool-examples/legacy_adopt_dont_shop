12f8a5545099d013eb2b9df5e07f09a658d1fc0c
Cydnee Owens <cks410@yahoo.com>
2020-12-11 19:01:19 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,29 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/", to: "welcome#index"
+  root 'welcome#index'
+  # get "/", to: "welcome#index"
 
-  get "/shelters", to: "shelters#index"
-  get "/shelters/new", to: "shelters#new"
-  get "/shelters/:id", to: "shelters#show"
-  delete "/shelters/:id", to: "shelters#destroy"
-  post "/shelters", to: "shelters#create"
-  get "/shelters/:id/edit", to: "shelters#edit"
-  patch "/shelters/:id", to: "shelters#update"
+  # resources :shelters 
+  # get "/shelters", to: "shelters#index"
+  # get "/shelters/new", to: "shelters#new"
+  # get "/shelters/:id", to: "shelters#show"
+  # delete "/shelters/:id", to: "shelters#destroy"
+  # post "/shelters", to: "shelters#create"
+  # get "/shelters/:id/edit", to: "shelters#edit"
+  # patch "/shelters/:id", to: "shelters#update"
 
+  resources :pets
+  # get "/pets", to: "pets#index"
+  # get "/pets/:id", to: "pets#show"
+  # get "/pets/:id/edit", to: "pets#edit"
+  # patch "/pets/:id", to: "pets#update"
+  # delete "/pets/:id", to: "pets#destroy"
 
-  get "/pets", to: "pets#index"
-  get "/pets/:id", to: "pets#show"
-  get "/pets/:id/edit", to: "pets#edit"
-  patch "/pets/:id", to: "pets#update"
-  delete "/pets/:id", to: "pets#destroy"
-
-  get "/shelters/:shelter_id/pets", to: "shelter_pets#index"
-  get "/shelters/:shelter_id/pets/new", to: "shelter_pets#new"
-  post "/shelters/:shelter_id/pets", to: "shelter_pets#create"
+  resources :shelters do 
+    resources :pets, controller: "shelter_pets", only: [:index, :new, :create]
+  end
+  # get "/shelters/:shelter_id/pets", to: "shelter_pets#index"
+  # get "/shelters/:shelter_id/pets/new", to: "shelter_pets#new"
+  # post "/shelters/:shelter_id/pets", to: "shelter_pets#create"
   get "/shelters/:id/pets/:id", to: "pets#show"
 end

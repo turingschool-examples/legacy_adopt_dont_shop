bd64f677f009c7af8e3bf06e7615a0a76d6f78f4
Grayson Cunningham <gecun289@gmail.com>
2020-12-11 17:32:05 -0700

    add form for creating a new application

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -5,46 +5,42 @@ RSpec.describe "New Application Page" do
     visit applications_new_path
 
     expect(page).to have_selector('form')
-    expect(page).to have_field(:applicant_name)
-    expect(page).to have_field(:applicant_address)
-    expect(page).to have_field(:applicant_city)
-    expect(page).to have_field(:applicant_state)
-    expect(page).to have_field(:applicant_zip)
-    expect(page).to have_field(:home_description)
-    expect(page).to have_button("Submit")
+    expect(page).to have_field("application[applicant_name]")
+    expect(page).to have_field("application[street_address]")
+    expect(page).to have_field("application[city]")
+    expect(page).to have_field("application[state]")
+    expect(page).to have_field("application[zip]")
+    expect(page).to have_button("Save Application")
   end
 
   it 'navigates to the new applications show page on submit' do
     visit applications_new_path
 
     fill_in(:applicant_name)
-    fill_in(:applicant_address)
-    fill_in(:applicant_city)
-    fill_in(:applicant_state)
-    fill_in(:applicant_zip)
-    fill_in(:home_description)
+    fill_in(:address)
+    fill_in(:city)
+    fill_in(:state)
+    fill_in(:zip)
     click_button("Submit")
 
     application = Application.first
     expect(page).to have_current_path("/applications/#{application.id}")
     expect(page).to have_content(application.applicant_name)
-    expect(page).to have_content(application.applicant_address)
-    expect(page).to have_content(application.applicant_city)
-    expect(page).to have_content(application.applicant_state)
-    expect(page).to have_content(application.applicant_zip)
-    expect(page).to have_content(application.home_description)
+    expect(page).to have_content(application.address)
+    expect(page).to have_content(application.city)
+    expect(page).to have_content(application.state)
+    expect(page).to have_content(application.zip)
     expect(page).to have_content("In Progress")
   end
 
   describe "visitors cannot submit incomplete applications" do
-    it 'name is required' do
+    it 'applicant_name is required' do
       visit applications_new_path
 
-      fill_in(:applicant_address)
-      fill_in(:applicant_city)
-      fill_in(:applicant_state)
-      fill_in(:applicant_zip)
-      fill_in(:home_description)
+      fill_in(:address)
+      fill_in(:city)
+      fill_in(:state)
+      fill_in(:zip)
       click_button("Submit")
 
       expect(page).to have_current_path(applications_new_path)
@@ -55,10 +51,9 @@ RSpec.describe "New Application Page" do
       visit applications_new_path
 
       fill_in(:applicant_name)
-      fill_in(:applicant_city)
-      fill_in(:applicant_state)
-      fill_in(:applicant_zip)
-      fill_in(:home_description)
+      fill_in(:city)
+      fill_in(:state)
+      fill_in(:zip)
       click_button("Submit")
 
       expect(page).to have_current_path(applications_new_path)
@@ -69,10 +64,9 @@ RSpec.describe "New Application Page" do
       visit applications_new_path
 
       fill_in(:applicant_name)
-      fill_in(:applicant_city)
-      fill_in(:applicant_state)
-      fill_in(:applicant_zip)
-      fill_in(:home_description)
+      fill_in(:city)
+      fill_in(:state)
+      fill_in(:zip)
       click_button("Submit")
 
       expect(page).to have_current_path(applications_new_path)
@@ -83,10 +77,9 @@ RSpec.describe "New Application Page" do
       visit applications_new_path
 
       fill_in(:applicant_name)
-      fill_in(:applicant_address)
-      fill_in(:applicant_city)
-      fill_in(:applicant_zip)
-      fill_in(:home_description)
+      fill_in(:address)
+      fill_in(:city)
+      fill_in(:zip)
       click_button("Submit")
 
       expect(page).to have_current_path(applications_new_path)
@@ -97,28 +90,13 @@ RSpec.describe "New Application Page" do
       visit applications_new_path
 
       fill_in(:applicant_name)
-      fill_in(:applicant_address)
-      fill_in(:applicant_city)
-      fill_in(:applicant_state)
-      fill_in(:home_description)
+      fill_in(:address)
+      fill_in(:city)
+      fill_in(:state)
       click_button("Submit")
 
       expect(page).to have_current_path(applications_new_path)
       expect(page).to have_content("Zip is a required field")
-    end
-
-    it 'home_description is required' do
-      visit applications_new_path
-
-      fill_in(:applicant_name)
-      fill_in(:applicant_address)
-      fill_in(:applicant_city)
-      fill_in(:applicant_state)
-      fill_in(:applicant_zip)
-      click_button("Submit")
-
-      expect(page).to have_current_path(applications_new_path)
-      expect(page).to have_content("home is a required field")
     end
   end
 end

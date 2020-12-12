fa11906e7ad30118267f9ae6f0e7eb6ae3f82a11
Vanessa Arruda <wanessaarruda@gmail.com>
2020-12-11 17:46:33 -0700

    Add MVC for story 1

--- spec/factories/pet.rb
+++ spec/factories/pet.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :pet do
     shelter
-    image ""
+    image {""}
     name { Faker::Creature::Dog.name}
-    approximate_age { Faker::Name.age}
+    approximate_age { Faker::Number.number(digits: 2)}
     description { ['Cat','Dog'].sample }
     adoptable { Faker::Boolean.boolean}
-    sex { Faker::Name.gender}
+    sex { Faker::Gender.binary_type.downcase}
   end
 end
--- spec/features/applications/application_spec.rb
+++ spec/features/applications/application_spec.rb
@@ -1,20 +1,28 @@
 require 'rails_helper'
 
 describe 'As a visitor' do
-  describe 'When I visit the show page' do
+  describe 'When I visit an application show page' do
     before :each do
-      application_1 = create(:application)
-      application_2 = create(:application)
-      application_3 = create(:application)
-      shelter_1 = create(:shelter)
-      shelter_2 = create(:shelter)
-      shelter_3 = create(:shelter)
-      pet_1 = create(:pet, :shelter: shelter_1)
-      pet_2 = create(:pet, :shelter: shelter_2)
-      pet_3 = create(:pet, :shelter: shelter_3)
+      @shelter_1 = create(:shelter)
+      @shelter_2 = create(:shelter)
+      @shelter_3 = create(:shelter)
+      @pet_1 = create(:pet, shelter: @shelter_1)
+      @pet_2 = create(:pet, shelter: @shelter_2)
+      @pet_3 = create(:pet, shelter: @shelter_3)
+      @application_1 = create(:application)
+      @application_2 = create(:application)
+      @application_3 = create(:application)
     end
-    it '' do
-      
+    it 'I can see the application and its attributes ' do
+    visit "/applications/#{@application_1.id}"
+    save_and_open_page
+      expect(page).to have_content(@application_1.applicant_name)
+      expect(page).to have_content(@application_1.applicant_address)
+      expect(page).to have_content(@application_1.city)
+      expect(page).to have_content(@application_1.state)
+      expect(page).to have_content(@application_1.zip)
+      expect(page).to have_content(@application_1.description)
+      expect(page).to have_content(@application_1.status)
     end
   end
 end

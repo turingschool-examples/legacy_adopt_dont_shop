6b85d34755447d16a695b05407ff79fefe6dbf53
Ely Hess <ely.hess@me.com>
2020-12-12 18:29:17 -0700

    Implement pending update

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationPetsController < ApplicationController
   def create
-    ApplicationPet.create!(application_id: params[:application_id],
+    @application = Application.find(params[:application_id])
+    @application.update(application_status: "Pending")
+    ApplicationPet.create(application_id: params[:application_id],
                             pet_id: params[:pet_id])
     redirect_to application_path(params[:application_id])
   end

881fae030db78fdea99190dd7f01672f53b88b88
awosborn <alanturing@example.coclear>
2020-12-13 11:14:29 -0700

    Add application validates presence of

--- spec/features/applications/index_spec.rb
+++ spec/features/applications/index_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
   describe 'As a visitor' do
     describe 'when I visit an applications index page' do
     it 'displays names of all applications' do 
-      joe = Application.create(name: 'Joe', street_address: "12 Broadway", state: "CO", zip: 80303) 
+      joe = Application.create!(name: 'Joe', street_address: "12 Broadway", city: 'Denver', state: "CO", zip: 80303) 
       visit '/applications'
 
       expect(page).to have_content(joe.name)

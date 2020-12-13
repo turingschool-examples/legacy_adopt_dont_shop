0f8985a2988549ac47d33afc823bff62fcebe120
Grayson Cunningham <gecun289@gmail.com>
2020-12-12 18:43:42 -0700

    add tests and instance method to approve applications

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,4 +3,12 @@ class Application < ApplicationRecord
   has_many :pets, through: :application_pets
 
   validates_presence_of :applicant_name, :street_address, :city, :state, :zip
+
+  def approve
+    if application_pets.all? {|pet| pet.status=="Approved"}
+      self[:status] = "Approved"
+    elsif application_pets.any? {|pet| pet.status=="Rejected"}
+      self[:status] = "Rejected"
+    end
+  end
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -19,4 +19,45 @@ RSpec.describe Application do
       expect(application.status).to eq("In Progress")
     end
   end
+
+  describe 'instance methods' do
+    it 'is approved when its pets are approved' do
+      application = create(:application, status: "Pending")
+      application_pet = create(:application_pet, application: application)
+      application_pet.approve
+      application.approve
+
+      expect(application.status).to eq("Approved")
+    end
+
+    it 'is approved when all of its pets are approved' do
+      application = create(:application, status: "Pending")
+      2.times {create(:application_pet, application: application)}
+
+      application.application_pets[0].approve
+      application.approve
+
+      expect(application.status).to eq("Pending")
+
+      application.application_pets[1].approve
+      application.approve
+
+      expect(application.status).to eq("Approved")
+    end
+
+    it 'is rejected if any of its pets are rejected' do
+      application = create(:application, status: "Pending")
+      2.times {create(:application_pet, application: application)}
+
+      application.application_pets[0].approve
+      application.approve
+
+      expect(application.status).to eq("Pending")
+
+      application.application_pets[1].reject
+      application.approve
+
+      expect(application.status).to eq("Rejected")
+    end
+  end
 end

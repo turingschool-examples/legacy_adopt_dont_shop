bc9142c7223b8e764774cf1b22db64e8a536fe76
awosborn <alanturing@example.coclear>
2020-12-13 13:20:08 -0700

    Add search bar and Pet search in modele

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -7,6 +7,18 @@ class Pet < ApplicationRecord
   validates :approximate_age, numericality: {
               greater_than_or_equal_to: 0
             }
-
   enum sex: [:female, :male]
+
+  def self.search(search)
+    if search
+      wanted_pet = Pet.find_by(name: search)
+        if wanted_pet
+          self.where(pet_id: wanted_pet)
+        else
+          @pets = Pet.all
+        end
+    else
+      @pets = Pet.all
+    end
+  end
 end

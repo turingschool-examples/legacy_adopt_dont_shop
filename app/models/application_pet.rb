b5f06b368db792f9634592f01c865747da4da832
Grayson Cunningham <gecun289@gmail.com>
2020-12-12 17:00:32 -0700

    add reject and approve methods to application_pet class

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -1,4 +1,12 @@
 class ApplicationPet < ApplicationRecord
   belongs_to :application
   belongs_to :pet
+
+  def approve
+    self[:status] = "Approved"
+  end
+
+  def reject
+    self[:status] = "Rejected"
+  end
 end

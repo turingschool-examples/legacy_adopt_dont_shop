fb3d2349afbcbea54b56043d2d8f275feb1907a2
Adam Etzion <adametzion90@gmail.com>
2020-12-13 16:30:58 -0700

    Add application show page spec

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,14 +1,27 @@
 require 'rails-helper'
 
 Rspec.describe "the Application index page" do
-    it "should display Applicant info" do
+    it "should display Application info" do
          shelter1 = FactoryBot.create(:shelter)
          pet1 = FactoryBot.create(:pet, shelter: shelter1)
          pet2 = FactoryBot.create(:pet, shelter: shelter1)
          applicant1 = FactoryBot.create(:applicant)
+         application1 = FactoryBot.create(:application, applicant: applicant1)
+
+         ApplicationPet.create!(pet: pet1, application: application1)
+         ApplicationPet.create!(pet: pet2, application: application1)
 
          visit "/applicants/#{applicant1.id}"
 
+         expect(page).to have_content(applicant1.name)
          expect(page).to have_content(applicant1.address)
          expect(page).to have_content(applicant1.address)
+         expect(page).to have_content(applicant1.city)
+         expect(page).to have_content(applicant1.zip)
+         expect(page).to have_content(application1.description)
+         expect(page).to have_content(application1.status)
+         expect(page).to have_content(pet1.name)
+         expect(page).to have_content(pet2.name)
+    end
+end
 

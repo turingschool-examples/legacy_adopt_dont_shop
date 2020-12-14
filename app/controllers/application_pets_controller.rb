abec271a207e6f6808094d111a8869afa33ec858
Grayson Myers <graymyers10@gmail.com>
2020-12-14 16:49:54 -0700

    Add application pet creation

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -2,4 +2,12 @@ class ApplicationPetsController < ApplicationController
   def index
     @pets = ApplicationPet.find_by_name(params[:search])
   end
+
+  def create
+    pet = Pet.find(params[:pet_id])
+    application = Application.find(params[:id])
+    relation = ApplicationPet.create(pet: pet, application: application)
+    redirect_to("/applications/#{params[:id]}")
+  end
+
 end

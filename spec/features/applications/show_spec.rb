8ae6da5750c436eeb8372e57063d1603924c8559
awosborn <alanturing@example.coclear>
2020-12-13 17:49:26 -0700

    Add test for Submit and Application

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -31,9 +31,26 @@ require 'rails_helper'
       visit "/applications/#{joe.id}"
       fill_in "Add a Pet to this Application", with:"#{puppy.name}"
       click_on "Search"
-      # require 'pry'; binding.pry
       click_link "Adopt this Pet"
 
+      expect(page).to have_content(puppy.name)
     end
+
+    it 'I can search for a pet and add it to the page'  do
+      joe = Application.create!(name: 'Joe', street_address: "12 Broadway", city: "Boulder", state: "CO", zip: 80303)
+      shelter = create(:shelter)
+      puppy = create(:pet, shelter: shelter)
+      kitteh = create(:pet, shelter: shelter)
+      pet_app = PetApplication.create!(application: joe, pet: puppy, pet:kitteh)
+
+      visit "/applications/#{joe.id}"
+      fill_in "Add a Pet to this Application", with:"#{puppy.name}"
+      click_on "Search"
+      click_link "Adopt this Pet"
+
+      expect(page).to have_button("Submit")
+      expect(page).to have_content("Pending")
+    end
+
   end
 end

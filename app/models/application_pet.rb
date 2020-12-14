a90d384abe455f19c28d03add956da9a9b13613e
Ely Hess <ely.hess@me.com>
2020-12-14 08:34:43 -0700

    Add all_pets_approved? method

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -1,4 +1,12 @@
 class ApplicationPet < ApplicationRecord
+  scope :is_approved, -> { where('status = ?', "Approved") }
+  scope :find_applications, -> (application_id) { where(application_id: application_id) }
   belongs_to :application
   belongs_to :pet
+
+  def self.all_pets_approved?(application_id)
+    find_applications(application_id).is_approved
+    .count == find_applications(application_id).count
+  end
+
 end

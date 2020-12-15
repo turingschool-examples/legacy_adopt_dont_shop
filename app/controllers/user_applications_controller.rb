9f8f63c15aec0e5784776a9a7be56f26e2cf27ec
Isabelle Villasenor <villasenori@outlook.com>
2020-12-14 20:35:24 -0700

    Complete user story 3

--- app/controllers/user_applications_controller.rb
+++ app/controllers/user_applications_controller.rb
@@ -4,14 +4,12 @@ class UserApplicationsController < ApplicationController
   end
 
   def new
-    @userapp = UserApplication.new
   end
 
   def create
     app = UserApplication.new(app_params)
     if app.save
       redirect_to user_application_path(app.id)
-      flash[:success] = nil
     else
       flash[:failure] = "Please Fill in all Required Fields"
       render :new
@@ -20,6 +18,6 @@ class UserApplicationsController < ApplicationController
 
   private
   def app_params
-    params.permit(:name, :address, :city, :state, :zip, :description)
+    params.permit(:name, :address, :city, :state, :zip, :description, :status)
   end
 end

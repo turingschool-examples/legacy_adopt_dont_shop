b34016bc74a85edba53dc147dfb04429e93ce213
Grayson Myers <graymyers10@gmail.com>
2020-12-14 20:08:49 -0700

    Add action for reviewing pets

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -2,4 +2,11 @@ class AdminApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def review
+    application_pet = ApplicationPet.where(pet_id: params[:pet_id],application_id: params[:id])
+    application_pet.status = params[:action]
+    application_pet.save
+    redirect("/admin/applications/#{params[:id]}")
+  end
 end

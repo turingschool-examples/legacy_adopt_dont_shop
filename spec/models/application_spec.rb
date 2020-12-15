78573de550fd9def133468861e068de9179b6dc4
Grayson Myers <graymyers10@gmail.com>
2020-12-14 20:35:54 -0700

    Add test for check_status

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -34,6 +34,43 @@ describe Application, type: :model do
 
       expect(@application1.submitted?).to eq(true)
     end
+
+    it "checks whether status is approved or rejected" do
+      pet1app1 = ApplicationPet.create(application: @application1, pet: @pet1)
+      pet2app1 = ApplicationPet.create(application: @application1, pet: @pet2)
+
+
+      pet3app2 = ApplicationPet.create(application: @application1, pet: @pet3)
+      pet2app2 = ApplicationPet.create(application: @application1, pet: @pet2)
+
+      @application1.status = "Pending"
+      @application2.status = "Pending"
+
+      @application1.save
+      @application2.save
+
+      pet1app1.status = "Approved"
+      pet3app2.status = "Approved"
+      pet1app1.save
+      pet3app2.save
+
+      @application1.check_status
+      @application2.check_status
+
+      expect(@application1.status).to eq("Pending")
+      expect(@application2.status).to eq("Pending")
+
+      pet2app1.status = "Rejected"
+      pet2app2.status = "Approved"
+      pet2app1.save
+      pet2app2.save
+
+      @application1.check_status
+      @application2.check_status
+
+      expect(@application1.status).to eq("Rejected")
+      expect(@application2.status).to eq("Approved")
+    end
   end
 
 end

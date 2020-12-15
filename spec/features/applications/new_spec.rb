b9031cc1d3cb812c253b6f8ae7cd492641a8c5c7
Grayson Myers <graymyers10@gmail.com>
2020-12-15 11:56:02 -0700

    Add test for new application page

---
+++ spec/features/applications/new_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+describe "New page" do
+
+  before :each do
+    @shelter1 = Shelter.create!(name: "Shady Shelter", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
+
+    @pet1 = @shelter1.pets.create!(image:"", name: "Thor", description: "dog", approximate_age: 2, sex: "male")
+    @pet2 = @shelter1.pets.create!(image:"", name: "Athena", description: "cat", approximate_age: 3, sex: "female")
+    @pet3 = @shelter1.pets.create!(image:"", name: "Zeus", description: "dog", approximate_age: 4, sex: "male")
+    visit "/applications/new"
+  end
+
+  it "has fields for information to fill out" do
+    expect(page).to have_field("Name")
+    expect(page).to have_field("Address")
+    expect(page).to have_field("City")
+    expect(page).to have_field("State")
+    expect(page).to have_field("Zip")
+    expect(page).to have_button("Submit")
+  end
+
+  it "creates a new application and redirects to the application's show page" do
+    fill_in "Name", with: "John Doe"
+    fill_in "Address", with: "1234 Some Street"
+    fill_in "City", with: "Denver"
+    fill_in "Zip", with: "80000"
+    click_on "Submit"
+
+    processed = current_path.split("/") 
+    expect(processed[0]).to eq("applications")
+    expect(processed[1].length).to eq(processed[1].to_i.to_s.length)
+
+    expect(page).to have_content("John Doe")
+    expect(page).to have_content("1234 Some Street")
+    expect(page).to have_content("Denver")
+    expect(page).to have_content("80000")
+  end
+end

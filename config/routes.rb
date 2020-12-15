af71dcd441815ff44e4d4a9426ab44fc6494a2b6
Grayson Myers <graymyers10@gmail.com>
2020-12-14 18:38:10 -0700

    Fix typo and make route restful

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,9 @@ Rails.application.routes.draw do
   get "/shelters/:id/pets/:id", to: "pets#show"
 
   get "/applications/:id", to: "applications#show"
+  patch "/applications/:id", to: "applications#update"
+  post "/applications/:id", to: "applications#submit"
+  get "/applications/:id/edit", to: "applications#edit"
 
   get "/applications/:id/pets", to: "application_pets#index"
   post "/applications/:id/pets/:pet_id", to: "application_pets#create"

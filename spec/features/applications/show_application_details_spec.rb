d15b237667b9c78f695713b86deb400cb3c9e165
Aiden Mendez <aidenmendez@gmail.com>
2020-12-15 16:54:01 -0700

    Test applicant can add pet to application - Add pet-id sections to pets search partial - Add section to app show page for pets applied for

--- spec/features/applications/show_application_details_spec.rb
+++ spec/features/applications/show_application_details_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe 'As a user' do
   describe "when I visit the Application show page" do
     before :each do
-      @shelter1 = Shelter.create(name: "Shady Shelter", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
-      @shelter2 = Shelter.create(name: "Silly Shelter", address: "123 Silly Ave", city: "Longmont", state: "CO", zip: 80012)
-      @shelter3 = Shelter.create(name: "Shell Shelter", address: "102 Shelter Dr.", city: "Commerce City", state: "CO", zip: 80022)
-      @pet1 = @shelter1.pets.create(image:"", name: "Thor", description: "dog", approximate_age: 2, sex: "male")
-      @pet2 = @shelter2.pets.create(image:"", name: "Athena", description: "cat", approximate_age: 3, sex: "female")
-      @pet3 = @shelter1.pets.create(image:"", name: "Zeus", description: "dog", approximate_age: 4, sex: "male")
+      @shelter1 = FactoryBot.create(:shelter)
+      @shelter2 = FactoryBot.create(:shelter)
+      @shelter3 = FactoryBot.create(:shelter)
+      @pet1 = FactoryBot.create(:pet)
+      @pet2 = FactoryBot.create(:pet)
+      @pet3 = FactoryBot.create(:pet)
 
       @application = Application.create(name: 'Billy Bob', 
         address: '123 W Street',
@@ -60,7 +60,6 @@ RSpec.describe 'As a user' do
 
     it "should show application details" do
       visit "/applications/#{@application.id}"
-      save_and_open_page
       expect(page).to have_content(@application.name)
       expect(page).to have_content(@application.address)
       expect(page).to have_content(@application.description)

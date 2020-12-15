ea566bbc8afdeedc9a843bb875f5ab10695b5724
Ely Hess <ely.hess@me.com>
2020-12-14 18:57:18 -0700

    Add any_pets_rejected test

--- spec/models/application_pets_spec.rb
+++ spec/models/application_pets_spec.rb
@@ -13,17 +13,17 @@ describe ApplicationPet, type: :model do
   end
 
   describe "class methods" do
-    it ".all_pets_approved?" do
-      shelter = Shelter.create!(name: "Pet Rescue", address: "123 Adoption Ln.", city: "Denver", state: "CO", zip: "80222")
-      pet1 = shelter.pets.create!(sex: :female, name: "Fluffy", approximate_age: 3, description: "super cute")
-      pet2 = shelter.pets.create!(sex: :female, name: "Floppy", approximate_age: 3, description: "super cute")
-      pet3 = shelter.pets.create!(sex: :female, name: "Borko", approximate_age: 3, description: "super cute")
+    before :each do
+      @shelter = Shelter.create!(name: "Pet Rescue", address: "123 Adoption Ln.", city: "Denver", state: "CO", zip: "80222")
+      @pet1 = @shelter.pets.create!(sex: :female, name: "Fluffy", approximate_age: 3, description: "super cute")
+      @pet2 = @shelter.pets.create!(sex: :female, name: "Floppy", approximate_age: 3, description: "super cute")
+      @pet3 = @shelter.pets.create!(sex: :female, name: "Borko", approximate_age: 3, description: "super cute")
 
-      pet5 = shelter.pets.create!(sex: :female, name: "lena", approximate_age: 6, description: "sort of cute")
-      pet6 = shelter.pets.create!(sex: :male, name: "ed", approximate_age: 1, description: "scruffy cute")
-      pet7 = shelter.pets.create!(sex: :female, name: "fark", approximate_age: 2, description: "mega cute")
+      @pet5 = @shelter.pets.create!(sex: :female, name: "lena", approximate_age: 6, description: "sort of cute")
+      @pet6 = @shelter.pets.create!(sex: :male, name: "ed", approximate_age: 1, description: "scruffy cute")
+      @pet7 = @shelter.pets.create!(sex: :female, name: "fark", approximate_age: 2, description: "mega cute")
 
-      abby = Application.create!(name: "Abby",
+      @abby = Application.create!(name: "Abby",
                                  street: "2222 6th st.",
                                  city: "Denver",
                                  state: "CO",
@@ -31,7 +31,7 @@ describe ApplicationPet, type: :model do
                                  application_status: "Pending",
                                  description: "I want these pets.")
 
-      frank = Application.create!(name: "Abby",
+      @frank = Application.create!(name: "Abby",
                                   street: "2222 6th st.",
                                   city: "Denver",
                                   state: "CO",
@@ -39,16 +39,25 @@ describe ApplicationPet, type: :model do
                                   application_status: "Pending",
                                   description: "I want these pets.")
 
-      ApplicationPet.create!(application: abby, pet: pet1, status: "Approved")
-      ApplicationPet.create!(application: abby, pet: pet2, status: "Approved")
-      ApplicationPet.create!(application: abby, pet: pet3, status: "Approved")
+      ApplicationPet.create!(application: @abby, pet: @pet1, status: "Approved")
+      ApplicationPet.create!(application: @abby, pet: @pet2, status: "Approved")
+      ApplicationPet.create!(application: @abby, pet: @pet3, status: "Approved")
 
-      ApplicationPet.create!(application: frank, pet: pet5, status: "Approved")
-      ApplicationPet.create!(application: frank, pet: pet6, status: "Approved")
-      ApplicationPet.create!(application: frank, pet: pet7, status: "Rejected")
+      ApplicationPet.create!(application: @frank, pet: @pet5, status: "Approved")
+      ApplicationPet.create!(application: @frank, pet: @pet6, status: "Approved")
+      ApplicationPet.create!(application: @frank, pet: @pet7, status: "Rejected")
 
-      expect(ApplicationPet.all_pets_approved?(abby.id)).to eq(true)
-      expect(ApplicationPet.all_pets_approved?(frank.id)).to eq(false)
+      expect(ApplicationPet.all_pets_approved?(@abby.id)).to eq(true)
+      expect(ApplicationPet.all_pets_approved?(@frank.id)).to eq(false)
+    end
+    it ".all_pets_approved?" do
+      expect(ApplicationPet.all_pets_approved?(@abby.id)).to eq(true)
+      expect(ApplicationPet.all_pets_approved?(@frank.id)).to eq(false)
+    end
+
+    it '.any_pets_rejected?' do
+      expect(ApplicationPet.any_pets_rejected?(@abby.id)).to eq(false)
+      expect(ApplicationPet.any_pets_rejected?(@frank.id)).to eq(true)
     end
   end
 end

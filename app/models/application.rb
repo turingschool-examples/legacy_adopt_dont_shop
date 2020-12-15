8cb326feea7d4452bcdaea4fed3055050e02a3e1
Saundra <saundracatalina@gmail.com>
2020-12-14 17:23:22 -0700

    Add validations for presence of attributes of application

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,11 @@
 class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+
+  validates_presence_of :name,
+                        :address,
+                        :city,
+                        :state,
+                        :zip,
+                        :description
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,12 @@ describe Application, type: :model do
     it {should have_many :pet_applications}
     it {should have_many(:pets).through(:pet_applications)}
   end
+  describe "attributes" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip) }
+    it { should validate_presence_of(:description) }
+  end
 end

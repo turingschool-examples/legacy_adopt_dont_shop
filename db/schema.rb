d78717d27b49a311728dbe3621343eaeb699372d
Grayson Myers <graymyers10@gmail.com>
2020-12-15 12:21:41 -0700

    Add more information about address to prevent destroying information

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_034428) do
+ActiveRecord::Schema.define(version: 2020_12_15_190835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 2020_12_15_034428) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "submitted", default: false
+    t.string "city"
+    t.string "state"
+    t.string "zip"
   end
 
   create_table "pets", force: :cascade do |t|

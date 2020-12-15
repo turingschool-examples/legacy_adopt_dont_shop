30328c39e0c23e257fe62974affa6c5a2e5c6755
Saundra <saundracatalina@gmail.com>
2020-12-14 21:15:16 -0700

    Add test for search button showing pet searched for on page

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -17,4 +17,36 @@ describe "Applications show page" do
     expect(page).to have_content("Pets Applied For:")
     expect(page).to have_content("Application Status: #{@application1.status}")
   end
+  describe "before application is submitted" do
+    it "can see a section to Add a Pet with a field to search pets" do
+      @application1 = Application.create!(
+        name: "Bob Ross", address: "123 Paint Spill Lane", city: "Denver",
+        state: "Colorado", zip: 80224, description: "I love animals")
+
+      visit "/applications/#{@application1.id}"
+
+      expect(page).to have_content("Add a Pet to this Application:")
+      expect(page).to have_field(:search)
+      expect(page).to have_button("Search")
+    end
+  end
+  describe "when I fill in the search field and click submit" do
+    it "brings me back to the application show page and I can see any Pet who matches search" do
+        application1 = Application.create!(
+          name: "Bob Ross", address: "123 Paint Spill Lane", city: "Denver",
+          state: "Colorado", zip: 80224, description: "I love animals")
+        shelter1 = Shelter.create!(name: "Shady Shelter", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
+        shelter2 = Shelter.create!(name: "Silly Shelter", address: "123 Silly Ave", city: "Longmont", state: "CO", zip: 80012)
+        pet1 = shelter1.pets.create!(image:"", name: "Thor", description: "dog", approximate_age: 2, sex: "male")
+        pet2 = shelter2.pets.create!(image:"", name: "Athena", description: "cat", approximate_age: 3, sex: "female")
+
+        visit "/applications/#{application1.id}"
+
+        fill_in :search, with: pet1.name
+        click_on "Search"
+
+        expect(current_path).to eq("/applications/#{application1.id}")
+        expect(page).to have_content(pet1.name)
+    end
+  end
 end

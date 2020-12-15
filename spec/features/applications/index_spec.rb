cb4f1223c00c92c4a750923e45799a83972c955e
Saundra <saundracatalina@gmail.com>
2020-12-14 18:27:52 -0700

    Add test link to Start an Application

--- spec/features/applications/index_spec.rb
+++ spec/features/applications/index_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
 describe "applications index page" do
-  it "has a link to Start an Application" do
-    visit '/applications'
-    
-    expect(page).to have_link("Start an Application")
-  end
+
 end

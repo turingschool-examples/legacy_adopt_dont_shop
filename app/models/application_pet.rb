5b5fdee4d77a181f1383c6446b3eb1644ed67b46
Grayson Myers <graymyers10@gmail.com>
2020-12-14 18:45:37 -0700

    Change search method to use partial matching

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -3,6 +3,6 @@ class ApplicationPet < ApplicationRecord
   belongs_to :application
 
   def self.find_by_name(search)
-    Pet.where("name='#{search}'")
+    Pet.where("name like ?", "%#{search}%")
   end
 end

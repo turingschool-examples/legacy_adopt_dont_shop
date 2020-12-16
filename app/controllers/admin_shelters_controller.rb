9ef20b189af97ae2cb8f750161ac617b27c303b6
Ely Hess <ely.hess@me.com>
2020-12-16 12:35:04 -0700

    Add @pending_shelters

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -1,6 +1,7 @@
 class AdminSheltersController < ApplicationController
   def index
     @shelters = Shelter.reverse_alphabetical
+    @pending_shelters = Shelter.pending_applications
   end
 
   def show

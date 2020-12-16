0410b4928b99f296381a0d5453af6284342529e9
Ely Hess <ely.hess@me.com>
2020-12-15 21:40:01 -0700

    Change select name => all

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,7 +2,7 @@ class Shelter < ApplicationRecord
   has_many :pets, dependent: :destroy
 
   def self.reverse_alphabetical
-    find_by_sql('SELECT name FROM shelters ORDER BY name DESC')
+    find_by_sql('SELECT * FROM shelters ORDER BY name DESC')
   end
 
 end

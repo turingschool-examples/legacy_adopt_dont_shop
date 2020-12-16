e3776c26478c3e4e062b6f534db83efee049e3bf
Ely Hess <ely.hess@me.com>
2020-12-15 20:47:00 -0700

    Add reverse_alphabetical method

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,8 @@
 class Shelter < ApplicationRecord
   has_many :pets, dependent: :destroy
+
+  def self.reverse_alphabetical
+    find_by_sql('SELECT name FROM shelters ORDER BY name DESC')
+  end
+
 end

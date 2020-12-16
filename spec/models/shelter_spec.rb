0a3e003eba43af758356787846a7fea4f81a76e3
Ely Hess <ely.hess@me.com>
2020-12-16 12:19:55 -0700

    Add pending_applications test

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -20,5 +20,27 @@ describe Shelter, type: :model do
       expect(Shelter.name_address(@shelter1.id)).to eq(@shelter1)
     end
 
+    it ".pending_applications" do
+      shelter = Shelter.create!(name: "Pet Rescue", address: "123 Adoption Ln.", city: "Denver", state: "CO", zip: "80222")
+      shelter2 = Shelter.create!(name: "Pet Rescue", address: "123 Adoption Ln.", city: "Denver", state: "CO", zip: "80222")
+
+      pet1 = shelter.pets.create!(sex: :female, name: "Fluffy", approximate_age: 3, description: "super cute")
+      pet2 = shelter2.pets.create!(sex: :female, name: "Floppy", approximate_age: 3, description: "super cute")
+      pet3 = shelter2.pets.create!(sex: :female, name: "Borko", approximate_age: 3, description: "super cute")
+
+      frank = Application.create!(name: "Abby",
+                                   street: "2222 6th st.",
+                                   city: "Denver",
+                                   state: "CO",
+                                   zip_code: 80214,
+                                   application_status: "Pending",
+                                   description: "I want these pets.")
+
+      app_pet1 = ApplicationPet.create!(application: frank, pet: pet1, status: "Approved")
+      app_pet2 = ApplicationPet.create!(application: frank, pet: pet2, status: "Approved")
+      app_pet3 = ApplicationPet.create!(application: frank, pet: pet3, status: "Pending")
+      
+      expect(shelter2.pending_applications.size).to eq(1)
+    end
   end
 end

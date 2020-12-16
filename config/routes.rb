06af0d396b6597289f1242be10e9405114c0b8f9
Saundra <saundracatalina@gmail.com>
2020-12-15 17:53:50 -0700

    Add resources for pet_applications routes

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,7 @@ Rails.application.routes.draw do
   get "/applications/new", to: "applications#new"
   get "/applications/:id", to: "applications#show"
   post "/applications", to: "applications#create"
+
+  # post "/pet_applications", to: "pet_applications#create"
+  resources :pet_applications 
 end

c25224beb1cfde14302f572eb92721e660025f37
Ely Hess <ely.hess@me.com>
2020-12-15 20:38:47 -0700

    Add admin_shelters index spec

---
+++ spec/features/admin_shelters/index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  describe 'when i visit the admin shelter index' do
+    before :each do
+      @shelter1 = Shelter.create!(name: "A", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
+      @shelter2 = Shelter.create!(name: "B", address: "123 Silly Ave", city: "Denver", state: "CO", zip: 80012)
+      @shelter3 = Shelter.create!(name: "C", address: "123 Silly Ave", city: "Denver", state: "CO", zip: 80012)
+    end
+
+    it 'Then I see all Shelters in the system listed in reverse alphabetical order by name' do
+      visit shelters_path
+
+      within "#shelter-#{@shelter3.id}" do
+        expect(page).to have_content(@shelter1.name)
+      end
+
+      within "#shelter-#{@shelter2.id}" do
+        expect(page).to have_content(@shelter1.name)
+      end
+
+      within "#shelter-#{@shelter1.id}" do
+        expect(page).to have_content(@shelter1.name)
+      end
+    end
+  end
+end

13aba4c8a2e11ba29dac79a73fdaf18268300b0a
Ely Hess <ely.hess@me.com>
2020-12-16 17:19:06 -0700

    Add test variables

--- spec/features/admin_shelters/show_spec.rb
+++ spec/features/admin_shelters/show_spec.rb
@@ -6,6 +6,32 @@ describe 'As a visitor' do
       @shelter1 = Shelter.create!(name: "A", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
       @shelter2 = Shelter.create!(name: "B", address: "123 Silly Ave", city: "Denver", state: "CO", zip: 80012)
       @shelter3 = Shelter.create!(name: "C", address: "123 Silly Ave", city: "Denver", state: "CO", zip: 80012)
+
+      @pet1 = @shelter1.pets.create!(sex: :female, name: "k", approximate_age: 3, description: "super cute")
+      @pet2 = @shelter2.pets.create!(sex: :female, name: "z", approximate_age: 3, description: "super cute")
+      @pet3 = @shelter1.pets.create!(sex: :female, name: "y", approximate_age: 3, description: "super cute")
+      @pet4 = @shelter1.pets.create!(sex: :male, name: "x", approximate_age: 3, description: "super cute")
+
+      @frank = Application.create!(name: "Frank",
+                                  street: "2222 6th st.",
+                                  city: "Denver",
+                                  state: "CO",
+                                  zip_code: 80214,
+                                  application_status: "Pending",
+                                  description: "I want these pets.")
+
+      @linda = Application.create!(name: "linda",
+                                  street: "2222 6th st.",
+                                  city: "Denver",
+                                  state: "CO",
+                                  zip_code: 80214,
+                                  application_status: "Approved",
+                                  description: "I want these pets.")
+
+      @app_pet1 = ApplicationPet.create!(application: @linda, pet: @pet1, status: "Approved")
+      @app_pet2 = ApplicationPet.create!(application: @linda, pet: @pet2, status: "Approved")
+      @app_pet3 = ApplicationPet.create!(application: @frank, pet: @pet3, status: "In Progress")
+      @app_pet4 = ApplicationPet.create!(application: @frank, pet: @pet4, status: "In Progress")
     end
 
     it 'Then I see the shelters attributes' do
@@ -32,5 +58,13 @@ describe 'As a visitor' do
       end
     end
 
+    it 'Then I see the action required section with pets in a pending state' do
+      visit admin_shelter_path(@shelter1)
+
+      within('#action-required') do
+        expect(page).to have_content(@pet3.name)
+        expect(page).to have_content(@pet4.name)
+      end
+    end
   end
 end

feae8d1468a36a5fa696c000512e6a49ca3ac2cf
Grayson Myers <graymyers10@gmail.com>
2020-12-16 20:29:21 -0700

    Update view to properly set dynamic class of li

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -1,5 +1,5 @@
 class AdminSheltersController < ApplicationController
   def index
-    @shelters = Shelter.find_by_sql("SELECT name FROM Shelters ORDER BY name DESC")
+    @shelters = Shelter.find_by_sql("SELECT name, id FROM Shelters ORDER BY name DESC")
   end
 end

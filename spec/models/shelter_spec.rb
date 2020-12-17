4f3f50f6d2ca0129a802c8a07b6ea5d2ace72145
Grayson Myers <graymyers10@gmail.com>
2020-12-16 20:45:03 -0700

    Add test for formatted_address method of shelter model

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -4,4 +4,11 @@ describe Shelter, type: :model do
   describe 'relationships' do
     it { should have_many :pets }
   end
+
+  describe "instance methods" do
+    it "formatted_address" do
+      shelter1 = Shelter.create!(name: "Shady Shelter", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
+      expect(shelter1.formatted_address).to eq("123 Shady Ave Denver, CO 80129")
+    end
+  end
 end

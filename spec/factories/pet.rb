710b81750d2eb6d37c7b1c6c1ebe93b777fc3fa7
Aiden Mendez <aidenmendez@gmail.com>
2020-12-16 21:44:37 -0700

    Add partial search term matching and test it

--- spec/factories/pet.rb
+++ spec/factories/pet.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :pet do
-    name              {Faker::Creature::Dog.name}
+    name              {Faker::Creature::Dog.unique.name}
     image             {Faker::Avatar.image}
     approximate_age   {rand(1..15)}
     shelter_id        {Shelter.all.sample.id}

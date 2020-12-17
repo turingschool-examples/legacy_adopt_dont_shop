e392d56980c65e3ca7041672fe6decb24fe6afd8
Grayson Myers <graymyers10@gmail.com>
2020-12-17 11:04:19 -0700

    Add pending shelter logic

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -1,6 +1,7 @@
 class AdminSheltersController < ApplicationController
   def index
     @shelters = Shelter.find_by_sql("SELECT name, id FROM Shelters ORDER BY name DESC")
+    @pending_shelters = Shelter.pending_shelters
   end
 
   def show

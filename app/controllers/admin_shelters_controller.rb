df345ddd5bb9aa33c6199b3555c4391ac5c4cc31
Grayson Myers <graymyers10@gmail.com>
2020-12-16 20:38:42 -0700

    Add show action for admin shelter controller

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -2,4 +2,8 @@ class AdminSheltersController < ApplicationController
   def index
     @shelters = Shelter.find_by_sql("SELECT name, id FROM Shelters ORDER BY name DESC")
   end
+
+  def show
+    @shelter = Shelter.find_by_sql("SELECT name, address, city, state, zip FROM Shelters WHERE id = '#{params[:id]}'")
+  end
 end

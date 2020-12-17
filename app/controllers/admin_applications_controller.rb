34181709297569d67126bed1fb05ff06d2601922
Grayson Myers <graymyers10@gmail.com>
2020-12-16 18:31:36 -0700

    Make approving pets set them to not be adoptable

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -6,7 +6,9 @@ class AdminApplicationsController < ApplicationController
   def review
     application_pet = ApplicationPet.where(pet_id: params[:pet],application_id: params[:id]).first
     application_pet.status = params[:decision]
+    application_pet.pet.adoptable = false if params[:decision] == "Approved"
     application_pet.save
+    application_pet.pet.save
     redirect_to "/admin/applications/#{params[:id]}"
   end
 end

b8dcb90890c3542775f1550e1a7d72dad37167d7
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-11 17:17:04 -0700

    Hand roll application/new route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get "/shelters/:id/edit", to: "shelters#edit"
   patch "/shelters/:id", to: "shelters#update"
 
+  get "/applications/new", to: "applications#new"
 
   get "/pets", to: "pets#index"
   get "/pets/:id", to: "pets#show"
@@ -20,5 +21,5 @@ Rails.application.routes.draw do
   get "/shelters/:shelter_id/pets", to: "shelter_pets#index"
   get "/shelters/:shelter_id/pets/new", to: "shelter_pets#new"
   post "/shelters/:shelter_id/pets", to: "shelter_pets#create"
-  get "/shelters/:id/pets/:id", to: "pets#show"
+  get "/shelters/:shelter_id/pets/:id", to: "pets#show"
 end

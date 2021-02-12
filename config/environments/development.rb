b17b11dee5e3ea4bea79c929b79e6eb8051f4f7f
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-11 17:42:07 -0700

    Update lines 64 through 68 with code for Heroku to function

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
 end

1e5de5578ab9aebbf7265442eb8106f2a99f9cdc
Kris Litman <duguy8@gmail.com>
2021-02-11 22:39:15 -0500

    Added migration for applications, created model test and feature test for application show page

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'As a vistor visiting the Applications show page' do
+  before :each do
+    @app1 = Application.create!(
+      name: "Guy",
+      street_address: "111 Wherever Ln",
+      city: "Great Town",
+      state: "OH",
+      zipcode: 11552,
+      status: "In Progress",
+      description: "I love pets"
+    )
+  end
+  it 'Shows Application attributes' do
+    visit "/applications/#{@app1.id}"
+
+    expect(page).to have_content(@app1.name)
+    expect(page).to have_content(@app1.street_address)
+    expect(page).to have_content(@app1.city)
+    expect(page).to have_content(@app1.state)
+    expect(page).to have_content(@app1.zipcode)
+    expect(page).to have_content(@app1.status)
+    expect(page).to have_content(@app1.description)
+  end
+end
---
+++ spec/models/application_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Application, type: :model do
+  describe 'relationships' do
+    it { should have_many :pets }
+  end
+end
+
+describe 'validations' do
+  it {should validate_presence_of :name}
+  it {should validate_presence_of :street_address}
+  it {should validate_presence_of :city}
+  it {should validate_presence_of :state}
+  it {should validate_presence_of :zipcode}
+  it {should validate_presence_of :status}
+  it {should validate_presence_of :description}
+end

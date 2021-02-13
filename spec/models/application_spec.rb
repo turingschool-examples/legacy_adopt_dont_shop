79189e0371b4409d8cddce176b2ef5472db0c073
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-13 07:52:26 -0700

    Add should have_many pets through pet_applications test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe Application, type: :model do
   describe "relationships" do
     it { should have_many :pet_applications }
+    it { should have_many(:pets).through(:pet_applications) }
   end
 end

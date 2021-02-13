7eea1cf21d7233adae1759fb6a4d27877d21313c
Angel Breaux <abreaux@detaso.com>
2021-02-12 20:46:59 -0600

    Modify applicant model

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,4 +1,10 @@
 class Applicant < ApplicationRecord
   has_many :pet_applicants
   has_many :pets, through: :pet_applicants
+
+  enum status: [:in_progress, :pending, :accepted, :rejected]
+
+  def full_address
+    "#{address} #{city}, #{state} #{zip}"
+  end
 end

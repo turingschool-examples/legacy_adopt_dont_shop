5c4f033ed3e2df3316581fcc21b5f9389c59cff3
Arika Turing <ablenker@gmail.com>
2021-02-12 18:48:30 -0700

    Joined pet app table to pets and applications

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Application, type: :model do
   describe 'relationships' do
     it { should have_many :pet_applications}
-    # it { should have_many(:pets).through(:pet_applications) }
+    it { should have_many(:pets).through(:pet_applications) }
   end
 
 end

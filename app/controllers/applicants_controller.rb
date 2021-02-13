c24117b66d754e71597f6e264922563cfc43bc87
Angel Breaux <abreaux@detaso.com>
2021-02-13 16:07:56 -0600

    Add flash notice

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -9,13 +9,17 @@ class ApplicantsController < ApplicationController
   def create
     applicant = Applicant.new(applicant_params)
 
-    applicant.save
-    redirect_to "/applicants/#{applicant.id}"
+    if applicant.save
+      redirect_to "/applicants/#{applicant.id}"
+    else
+      flash[:notice] = "Applicant not created: Required information missing."
+      redirect_to "/applicants/new"
+    end
   end
-end
 
-private
+  private
 
   def applicant_params
     params.permit(:name, :address, :city, :state, :zip)
   end
+end

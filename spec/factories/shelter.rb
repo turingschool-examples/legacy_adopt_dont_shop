0b056ebeab047750e2548b1cf09ddbe3b9231ab5
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-13 15:02:22 -0700

    Create factory bot for shelter

--- spec/factories/shelter.rb
+++ spec/factories/shelter.rb
@@ -1,27 +1,18 @@
-# FactoryBot.define do
-  # factory :shelter do
-  #   name { "Shelter "}
-  #   address {"1245 Main Street"}
-  #   city {"Town-Town"}
-  #   state {"AL"}
-  #   zip {"00001"}
-  # end
-  #
-  # factory :admin, class: Pet do
-  #   sequence(:image) {|n| ""}
-  #   sequence(:name) {|n| "Zeus #{n}"}
-  #   sequence(:description) {|n| "dog #{n}"}
-  #   sequence(:approximate_age) {|n| n+4}
-  #   sequence(:sex) {|n| "male"}
-  #   shelter
-  # end
+FactoryBot.define do
+  factory :shelter do
+    name {"Shelter"}
+    address {"2222 Route Street"}
+    city {"ThisCity"}
+    state {"ThisState"}
+    zip {"22222"}
+  end
+end
 
-  # factory :pet do
-  #   image {""}
-  #   name {"Zeus"}
-  #   description {"dog"}
-  #   approximate_age {4}
-  #   sex {"male"}
-  #   shelter
-  # end
+
+# create_table "shelters", force: :cascade do |t|
+#   t.string "name"
+#   t.string "address"
+#   t.string "city"
+#   t.string "state"
+#   t.integer "zip"
 # end

4bd62beedc54384829b6da2c53de49d4f847f86a
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-13 16:53:03 -0500

    Complete form view with passing test

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -5,10 +5,25 @@ class ApplicationsController < ApplicationController
   end
 
   def show
-    binding.pry
     @application = Application.find(params[:id])
   end
 
   def new
+  end
+
+  def create
+    application = Application.new({
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      street_address: params[:street_address],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip],
+      status: "Pending",
+    })
+
+    application.save
+
+    redirect_to "/applications/#{application.id}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,8 @@ Rails.application.routes.draw do
   get "/applications", to: "applications#index"
   get "applications/new", to: "applications#new"
   get "/applications/:id", to: "applications#show"
+  post "applications", to: "applications#create"
+  
 
 
 end

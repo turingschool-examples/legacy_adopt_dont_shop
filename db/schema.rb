06a037d85f5b3feb979c159b0f3346297d5da362
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-12 20:56:46 -0700

    Schema auto updated for correction of p_pet_application correction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_012419) do
+ActiveRecord::Schema.define(version: 2021_02_13_034520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2021_02_13_012419) do
     t.string "reason"
     t.string "names_of_pets"
     t.string "application_status"
+  end
+
+  create_table "pet_applications", force: :cascade do |t|
+    t.bigint "application_id"
+    t.bigint "pet_id"
+    t.index ["application_id"], name: "index_pet_applications_on_application_id"
+    t.index ["pet_id"], name: "index_pet_applications_on_pet_id"
   end
 
   create_table "pets", force: :cascade do |t|
@@ -45,5 +52,7 @@ ActiveRecord::Schema.define(version: 2021_02_13_012419) do
     t.integer "zip"
   end
 
+  add_foreign_key "pet_applications", "applications"
+  add_foreign_key "pet_applications", "pets"
   add_foreign_key "pets", "shelters"
 end

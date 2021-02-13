5812e6acef2027542ad27388bd514c5ab742db61
Wil McCauley <wmccauley724@gmail.com>
2021-02-13 10:27:24 -0700

    Remove gigrate and uncomment enum

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,3 @@
 class Application < ApplicationRecord
-  # enum status: ['In Progress,', 'Pending', 'Accepted', 'Rejected']
+  enum status: ['In Progress', 'Pending', 'Accepted', 'Rejected']
 end
-
-rails g migration CreateApplications name:string address:string city:string state:string zip:integer description:text status:integer

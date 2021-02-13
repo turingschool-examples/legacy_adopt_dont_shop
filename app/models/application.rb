1f45b08bc06a16b261159596f265853433687a98
Wil McCauley <wmccauley724@gmail.com>
2021-02-12 17:38:04 -0700

    Add pickup code for enum and gmigration

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,5 @@
 class Application < ApplicationRecord
+  # enum status: ['In Progress,', 'Pending', 'Accepted', 'Rejected']
 end
+
+rails g migration CreateApplications name:string address:string city:string state:string zip:integer description:text status:integer

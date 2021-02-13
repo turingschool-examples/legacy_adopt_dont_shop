76063d44f04664932859fa3c4abc90deb59a7ad7
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-12 17:46:23 -0700

    Add factories directory for factory_bot_rails

---
+++ spec/factories/shelter.rb
@@ -0,0 +1,27 @@
+# FactoryBot.define do
+  # factory :shelter do
+  #   name { "Shelter "}
+  #   address {"1245 Main Street"}
+  #   city {"Town-Town"}
+  #   state {"AL"}
+  #   zip {"00001"}
+  # end
+  #
+  # factory :admin, class: Pet do
+  #   sequence(:image) {|n| ""}
+  #   sequence(:name) {|n| "Zeus #{n}"}
+  #   sequence(:description) {|n| "dog #{n}"}
+  #   sequence(:approximate_age) {|n| n+4}
+  #   sequence(:sex) {|n| "male"}
+  #   shelter
+  # end
+
+  # factory :pet do
+  #   image {""}
+  #   name {"Zeus"}
+  #   description {"dog"}
+  #   approximate_age {4}
+  #   sex {"male"}
+  #   shelter
+  # end
+# end

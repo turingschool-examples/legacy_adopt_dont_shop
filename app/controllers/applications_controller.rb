5e9bd6c7f76f3c3cb7a807ae4cc67c3874231acd
Wil McCauley <wmccauley724@gmail.com>
2021-02-13 16:03:13 -0700

    Add strong params, add create and show actions

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,24 @@ class ApplicationsController < ApplicationController
   def index
     @applications = Application.all
   end
+
+  def new
+  end
+
+  def create
+    application = Application.new(application_params)
+    application.save
+
+    redirect_to "/applications/#{application.id}"
+  end
+
+  def show
+    @application = Application.find(params[:id])
+  end
+
+  private
+  def application_params
+    # params.require(:exhibit)
+    params.permit(:name, :address, :city, :state, :zip, :description)
+  end
 end

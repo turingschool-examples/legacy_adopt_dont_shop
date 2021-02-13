4191be4430ed63c9661ea7858feb40308453bfae
Brisa Garcia <brisagarciaglz@gmail.com>
2021-02-13 15:28:10 -0700

    Modify model test

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Application, type: :model do
-  describe "relationships" do
-    it {should have_many:pet_applications}
+  # describe 'validations' do
+    # it { should validate_presence_of :}
+  # end
+
+  describe 'relationships' do
+    it {should have_many :pet_applications}
   end
+
+  # describe 'instance methods' do
+  # end
 end

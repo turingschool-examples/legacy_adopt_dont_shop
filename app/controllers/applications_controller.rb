7aa37e03a7c65267325ac26cb7cf0f43557bf31a
Dominic Padula <domopadula2192@gmail.com>
2021-02-13 11:18:05 -0700

    add new and create methods to application controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,4 +7,20 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new
+    @application = Application.new
+  end
+
+  def create
+    application = Application.new(application_params)
+    application.application_status = "In Progress"
+    application.save
+    redirect_to "/applications/#{application.id}"
+  end
+
+  private
+  def application_params
+    params.require(:application).permit(:name, :street, :city, :state, :zip_code, :description)
+  end
 end

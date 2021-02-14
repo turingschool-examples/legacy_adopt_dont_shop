d28c735ed754299ba179730cfff09294c1fcf05e
Jenny Branham <branhamjenny@gmail.com>
2021-02-14 12:23:50 -0600

    Add status to application pets

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -2,4 +2,6 @@ class ApplicationPet < ApplicationRecord
   belongs_to :application
   belongs_to :pet
   validates_presence_of :application_id, :pet_id
+
+  enum status: [:accepted, :rejected]
 end
--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -1,26 +1,42 @@
 require 'rails_helper'
 
 describe ApplicationPet, type: :model do
+  before :each do
+    ApplicationPet.destroy_all
+    Pet.destroy_all
+    Shelter.destroy_all
+    Application.destroy_all
+    shelter = create(:shelter, id: 1)
+    @pet1 = create(:pet, id: 1, shelter_id: 1)
+    @pet2 = create(:pet, id: 2, shelter_id: 1, name: "onyx")
+    @application = create(:application, id: 1)
+    @application_pet = create(:application_pet, application_id: 1, pet_id: 1)
+  end
   describe 'relationships' do
     it { should belong_to :application }
     it { should belong_to :pet }
   end
 
   describe 'validations' do
-    ApplicationPet.destroy_all
-    Pet.destroy_all
-    Shelter.destroy_all
-    Application.destroy_all
-
-    shelter = Shelter.create!(name: 'Pet Rescue', address: '123 Adoption Ln.', city: 'Denver', state: 'CO', zip: '80222')
-    pet = shelter.pets.create!(name: "Fluffy", approximate_age: 3, sex: 'male', description: 'super cute')
-    application = Application.create!(first_name: 'Jenny', last_name: 'Branham',
-                                      address: '123 Adoption Ln.', city: 'Denver',
-                                      state: 'CO', zipcode: '80222',
-                                      description: 'description of application', status: 'in_progress')
-    ApplicationPet.create!(application_id: application.id, pet_id: pet.id)
-
     it {should validate_presence_of :application_id}
     it {should validate_presence_of :pet_id}
+  end
+
+  it 'status can be nil by default' do
+    expect(@application_pet.status).to eq(nil)
+    expect(@application_pet.accepted?).to eq(false)
+    expect(@application_pet.rejected?).to eq(false)
+  end
+  it 'status can be accepted' do
+    application_pet = create(:application_pet, application_id: 1, pet_id: 1, status: :accepted)
+    expect(application_pet.status).to eq("accepted")
+    expect(application_pet.accepted?).to eq(true)
+    expect(application_pet.rejected?).to eq(false)
+  end
+  it 'status can be rejected' do
+    application_pet = create(:application_pet, application_id: 1, pet_id: 1, status: :rejected)
+    expect(application_pet.status).to eq('rejected')
+    expect(application_pet.accepted?).to eq(false)
+    expect(application_pet.rejected?).to eq(true)
   end
 end

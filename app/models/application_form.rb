055a14b267885a34d3937686bae6890264b28eec
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-14 12:44:41 -0700

    Add validations for new application sad path

--- app/models/application_form.rb
+++ app/models/application_form.rb
@@ -1,4 +1,10 @@
 class ApplicationForm < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :state, presence: true
+  validates :city, presence: true
+  validates :zip_code, presence: true
 end

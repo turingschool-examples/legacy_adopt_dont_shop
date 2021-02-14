9be35899f3c28df252342c13ef30f7a1d2549f63
Wil McCauley <wmccauley724@gmail.com>
2021-02-14 13:25:18 -0700

    Add validations for form fields to enable flash routing

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,7 @@
 class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+  validates :name, :address, :city, :state, :zip, presence: true
+
   enum status: ['In Progress', 'Pending', 'Accepted', 'Rejected']
 end

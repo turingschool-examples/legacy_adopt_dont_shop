9af6ca6edfe2d5fec3119cd8879ecbe171826bb1
Katy La Tour <klatour324@gmail.com>
2021-02-14 13:03:17 -0600

    Add validations for column attributes of Applications table

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,7 @@
 class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+
+  validates :first_name, :last_name, :address, :city, :state, :zip,
+            :case_for_adoption, presence: true
 end

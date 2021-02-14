e8aa379c231763a0735d38d2cccd38396c68d0d8
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-13 18:39:08 -0700

    Add route for new application

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,6 @@ Rails.application.routes.draw do
   get "/shelters/:id/pets/:id", to: "pets#show"
 
   get "/applications", to: "application_forms#index"
+  get "/applications/new", to: "application_forms#new"
   get "/applications/:id", to: "application_forms#show"
 end

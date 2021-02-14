de11b410f94abf8b64b594212dff2495899544ac
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-14 10:02:29 -0700

    Add action for new and create

--- app/controllers/application_forms_controller.rb
+++ app/controllers/application_forms_controller.rb
@@ -8,6 +8,15 @@ class ApplicationFormsController < ApplicationController
   end
 
   def new
+  end
 
-  end 
+  def create
+    ApplicationForm.create(application_params)
+    redirect_to '/applications'
+  end
+
+  private
+  def application_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :description, :reviewed, :accepted, :created_at, :updated_at)
+  end
 end

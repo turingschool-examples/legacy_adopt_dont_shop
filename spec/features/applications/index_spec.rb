a4b5456541debbab37e09686634e6df2e5a26e0b
Brisa Garcia <brisagarciaglz@gmail.com>
2021-02-13 17:16:25 -0700

    Userstory 1-mvc

---
+++ spec/features/applications/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+  describe 'As a visitor' do
+    describe 'when I visit an applications index page' do
+    it 'I see list of all applicants' do
+      applicant1 = Application.create!(name: 'Josh', street_address: "123 Main", state: "CO", zip_code: 80205, description: "I like furry pets")
+      visit '/applications'
+
+      expect(page).to have_content(applicant1.name)
+    end
+  end
+end

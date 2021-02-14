5536e5f69da64b8908a93f2bd285c6ee976faf96
Wil McCauley <wmccauley724@gmail.com>
2021-02-14 13:30:41 -0700

    Remove description field from initial app form

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -11,10 +11,26 @@ RSpec.describe 'the new application form page' do
     fill_in 'city', with: 'Golden'
     fill_in 'state', with: 'CO'
     fill_in 'zip', with: 80401
-    fill_in 'description', with: 'Looking for another cat to be friends with a new kitten'
     click_button 'Submit'
 
     app = Application.find_by(name: 'Tony S')
     expect(current_path).to eq("/applications/#{app.id}")
+
+    expect(page).to have_content('334 Ridge Road')
+    expect(page).to have_content('Golden')
+    expect(page).to have_content('CO')
+    expect(page).to have_content('80401')
+    expect(page).to have_content('In Progress')
+  end
+
+  describe 'as a visitor' do
+    it 'I cannot submit a blank form' do
+      visit '/applications/new'
+
+      click_button 'Submit'
+
+      expect(page).to have_content('Application could not be created, please fill in all fields!')
+      expect(current_path).to eq('/applications/new')
+    end
   end
 end

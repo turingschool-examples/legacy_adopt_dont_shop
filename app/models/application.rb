3a6db1c6af6c0ca568d13ec12a166dea9b2b9354
Brisa Garcia <brisagarciaglz@gmail.com>
2021-02-15 12:20:32 -0700

    Add status

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,5 @@
 class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
-  validates_presence_of :name, :street_address, :state, :city, :zip_code, :description
-
+  validates_presence_of :name, :street_address, :state, :city, :zip_code, :description, :status
 end

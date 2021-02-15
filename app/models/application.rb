34424aed1fd8ec469928f40b3226fe7d1a682bce
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-15 10:36:28 -0500

    Add validates presence of name, address, zip

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,11 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
 end

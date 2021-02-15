ca3efd2bb834bb90d39dedbd1a0808bde0e4a4de
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-15 15:08:46 -0600

    Update status attribute to be In Progress by default.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_195625) do
+ActiveRecord::Schema.define(version: 2021_02_15_210336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_02_14_195625) do
     t.string "state"
     t.integer "zip"
     t.string "description"
-    t.string "status"
+    t.string "status", default: "In Progress"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

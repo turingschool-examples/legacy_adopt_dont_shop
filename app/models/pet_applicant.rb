7fe8bc3b6d58bd118b02c3615a96dcbf016fbd82
Angel Breaux <abreaux@detaso.com>
2021-02-15 15:42:50 -0600

    Create pet applicant approved method

--- app/models/pet_applicant.rb
+++ app/models/pet_applicant.rb
@@ -1,4 +1,13 @@
 class PetApplicant < ApplicationRecord
+  validates_presence_of :pet_id, :applicant_id
   belongs_to :pet
   belongs_to :applicant
+
+  def self.find_by(pet_id, applicant_id)
+    where(pet: pet_id).where(applicant: applicant_id).take
+  end
+
+  def self.pet_applicant_approved?(pet_id, applicant_id)
+    where(pet: pet_id).where(applicant: applicant_id).take.is_pet_approved
+  end
 end

67fb14f787f77233901be695ffb8bd6ed6ec1324
Angel Breaux <abreaux@detaso.com>
2021-02-15 16:43:41 -0600

    Create approve or rejected method

--- app/models/pet_applicant.rb
+++ app/models/pet_applicant.rb
@@ -8,6 +8,16 @@ class PetApplicant < ApplicationRecord
   end
 
   def self.pet_applicant_approved?(pet_id, applicant_id)
-    where(pet: pet_id).where(applicant: applicant_id).take.is_pet_approved
+    find_by(pet_id, applicant_id).is_pet_approved
+  end
+
+  def self.approve_or_rejected(pet_id, applicant_id)
+    if !pet_applicant_approved?(pet_id, applicant_id).nil?
+      if pet_applicant_approved?(pet_id, applicant_id)
+        'Approved'
+      else
+        'Rejected'
+      end
+    end
   end
 end

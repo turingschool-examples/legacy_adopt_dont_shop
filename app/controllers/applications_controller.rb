2af182fb78a2578c22d602d90260a3180bb726ab
Katy La Tour <klatour324@gmail.com>
2021-02-14 19:56:17 -0600

    Create update action for applications_controller and association to add pets to existing application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -17,7 +17,13 @@ class ApplicationsController < ApplicationController
     end
   end
 
+  def update
+    application = Application.find(params[:id])
+    pet = Pet.find(params[:pet_id])
+    application.pets << pet
 
+    redirect_to "/applications/#{application.id}"
+  end
 
   private
 

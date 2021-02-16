8783e560098e33a877687265568d60865e808e8b
Trevor Suter <Trevorsuter@icloud.com>
2021-02-16 16:45:49 -0700

    created a before each do to shorten test length

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,269 +3,145 @@ require 'rails_helper'
 RSpec.describe "Applications Show page", type: :feature do
 
   describe "When I visit an application's show page" do
-    it "can see all of it's attributes" do
-      ddfl = Shelter.create!(
+    before :each do
+      @ddfl = Shelter.create!(
         name: "Denver Dumb Friends League",
         address: "123 Doggie Lane",
         city: "Denver",
         state: "CO",
         zip: 80246
       )
-      rico = ddfl.pets.create!(
+      @rico = @ddfl.pets.create!(
         name: "Rico",
         approximate_age: 4,
         description: "Staffordshire Terrier",
         sex: "male"
       )
-      saki = ddfl.pets.create!(
+      @saki = @ddfl.pets.create!(
         name: "Saki",
         approximate_age: 5,
         description: "mutt",
         sex: "female"
       )
-      trevor = Application.create!(
+      @trevor = Application.create!(
         name: "Trevor Suter",
         street_address: "1275 Birch Lane",
         city: "Denver",
         state: "CO",
         zip: 80220
       )
-      maddie = Application.create!(
+      @maddie = Application.create!(
         name: "Maddie Suter",
         street_address: "12 Birch Lane",
         city: "Denver",
         state: "CO",
         zip: 80246
       )
-      ApplicationPet.create!(application: trevor, pet: rico)
-      ApplicationPet.create!(application: trevor, pet: saki)
-      ApplicationPet.create!(application: maddie, pet: saki)
-      visit "/applications/#{trevor.id}"
-      expect(page).to have_content(trevor.name)
-      expect(page).to have_content(trevor.street_address)
-      expect(page).to have_content(trevor.city)
-      expect(page).to have_content(trevor.state)
-      expect(page).to have_content(trevor.zip)
-      expect(page).to have_content(rico.name)
-      expect(page).to have_content(saki.name)
-      expect(page).to have_content(trevor.status)
+    end
+    it "can see all of it's attributes" do
+      ApplicationPet.create!(application: @trevor, pet: @rico)
+      ApplicationPet.create!(application: @trevor, pet: @saki)
+      ApplicationPet.create!(application: @maddie, pet: @saki)
 
-      visit "/applications/#{maddie.id}"
-      expect(page).to have_content(maddie.name)
-      expect(page).to have_content(maddie.street_address)
-      expect(page).to have_content(maddie.city)
-      expect(page).to have_content(maddie.state)
-      expect(page).to have_content(maddie.zip)
-      expect(page).to have_content(saki.name)
-      expect(page).to have_content(maddie.status)
+      visit "/applications/#{@trevor.id}"
+
+      expect(page).to have_content(@trevor.name)
+      expect(page).to have_content(@trevor.street_address)
+      expect(page).to have_content(@trevor.city)
+      expect(page).to have_content(@trevor.state)
+      expect(page).to have_content(@trevor.zip)
+      expect(page).to have_content(@rico.name)
+      expect(page).to have_content(@saki.name)
+      expect(page).to have_content(@trevor.status)
+
+      visit "/applications/#{@maddie.id}"
+
+      expect(page).to have_content(@maddie.name)
+      expect(page).to have_content(@maddie.street_address)
+      expect(page).to have_content(@maddie.city)
+      expect(page).to have_content(@maddie.state)
+      expect(page).to have_content(@maddie.zip)
+      expect(page).to have_content(@saki.name)
+      expect(page).to have_content(@maddie.status)
     end
 
     it 'has links to the pets on the application' do
-      ddfl = Shelter.create!(
-        name: "Denver Dumb Friends League",
-        address: "123 Doggie Lane",
-        city: "Denver",
-        state: "CO",
-        zip: 80246
-      )
-      rico = ddfl.pets.create!(
-        name: "Rico",
-        approximate_age: 4,
-        description: "Staffordshire Terrier",
-        sex: "male"
-      )
-      saki = ddfl.pets.create!(
-        name: "Saki",
-        approximate_age: 5,
-        description: "mutt",
-        sex: "female"
-      )
-      trevor = Application.create!(
-        name: "Trevor Suter",
-        street_address: "1275 Birch Lane",
-        city: "Denver",
-        state: "CO",
-        zip: 80220
-      )
-      ApplicationPet.create!(application: trevor, pet: rico)
-      ApplicationPet.create!(application: trevor, pet: saki)
-      visit "/applications/#{trevor.id}"
-      expect(page).to have_link("#{rico.name}", href: "/pets/#{rico.id}")
-      expect(page).to have_link("#{saki.name}", href: "/pets/#{saki.id}")
+      ApplicationPet.create!(application: @trevor, pet: @rico)
+      ApplicationPet.create!(application: @trevor, pet: @saki)
+
+      visit "/applications/#{@trevor.id}"
+
+      expect(page).to have_link("#{@rico.name}", href: "/pets/#{@rico.id}")
+      expect(page).to have_link("#{@saki.name}", href: "/pets/#{@saki.id}")
 
     end
     it 'has a field to search for adoptable pets' do
-      ddfl = Shelter.create!(
-        name: "Denver Dumb Friends League",
-        address: "123 Doggie Lane",
-        city: "Denver",
-        state: "CO",
-        zip: 80246
-      )
-      rico = ddfl.pets.create!(
-        name: "Rico",
-        approximate_age: 4,
-        description: "Staffordshire Terrier",
-        sex: "male"
-      )
-      trevor = Application.create!(
-        name: "Trevor Suter",
-        street_address: "1275 Birch Lane",
-        city: "Denver",
-        state: "CO",
-        zip: 80220
-      )
+      visit "/applications/#{@trevor.id}"
 
-      visit "/applications/#{trevor.id}"
       fill_in "Search pets", with: "Rico"
       click_on("search")
-      expect(page).to have_content(rico.name)
+
+      expect(page).to have_content(@rico.name)
     end
 
     it 'can add a pet to the application' do
-      ddfl = Shelter.create!(
-        name: "Denver Dumb Friends League",
-        address: "123 Doggie Lane",
-        city: "Denver",
-        state: "CO",
-        zip: 80246
-      )
-      rico = ddfl.pets.create!(
-        name: "Rico",
-        approximate_age: 4,
-        description: "Staffordshire Terrier",
-        sex: "male"
-      )
-      saki = ddfl.pets.create!(
-        name: "Saki",
-        approximate_age: 5,
-        description: "mutt",
-        sex: "female"
-      )
-      trevor = Application.create!(
-        name: "Trevor Suter",
-        street_address: "1275 Birch Lane",
-        city: "Denver",
-        state: "CO",
-        zip: 80220
-      )
+      visit "/applications/#{@trevor.id}"
 
-      visit "/applications/#{trevor.id}"
       fill_in "Search pets", with: "Rico"
       click_on("search")
       click_on("Adopt This Pet")
-      expect(trevor.pets).to include(rico)
-      expect(page).to have_link("#{rico.name}", href: "/pets/#{rico.id}")
+
+      expect(@trevor.pets).to include(@rico)
+      expect(page).to have_link("#{@rico.name}", href: "/pets/#{@rico.id}")
     end
 
     it 'can submit an application' do
-      ddfl = Shelter.create!(
-        name: "Denver Dumb Friends League",
-        address: "123 Doggie Lane",
-        city: "Denver",
-        state: "CO",
-        zip: 80246
-      )
-      rico = ddfl.pets.create!(
-        name: "Rico",
-        approximate_age: 4,
-        description: "Staffordshire Terrier",
-        sex: "male"
-      )
-      saki = ddfl.pets.create!(
-        name: "Saki",
-        approximate_age: 5,
-        description: "mutt",
-        sex: "female"
-      )
-      trevor = Application.create!(
-        name: "Trevor Suter",
-        street_address: "1275 Birch Lane",
-        city: "Denver",
-        state: "CO",
-        zip: 80220
-      )
+      visit "/applications/#{@trevor.id}"
 
-      visit "/applications/#{trevor.id}"
       fill_in "Search pets", with: "Rico"
       click_on("search")
       click_on("Adopt This Pet")
-      visit "/applications/#{trevor.id}"
+
+      visit "/applications/#{@trevor.id}"
+
       fill_in "description", with: "I love Dogs"
       click_on("submit")
+
       expect(page).to_not have_selector("submit")
-      expect(page).to have_content(trevor.name)
+      expect(page).to have_content(@trevor.name)
       expect(page).to have_content("Pending")
       expect(page).to_not have_content("In Progress")
     end
 
     it 'can find partial pet name matches' do
-      ddfl = Shelter.create!(
-        name: "Denver Dumb Friends League",
-        address: "123 Doggie Lane",
-        city: "Denver",
-        state: "CO",
-        zip: 80246
-      )
-      rico = ddfl.pets.create!(
-        name: "Rico",
-        approximate_age: 4,
-        description: "Staffordshire Terrier",
-        sex: "male"
-      )
-      ricoboy = ddfl.pets.create!(
+      ricoboy = @ddfl.pets.create!(
         name: "Ricoboy",
         approximate_age: 4,
         description: "Staffordshire Terrier",
         sex: "male"
       )
-      trevor = Application.create!(
-        name: "Trevor Suter",
-        street_address: "1275 Birch Lane",
-        city: "Denver",
-        state: "CO",
-        zip: 80220
-      )
+      visit "/applications/#{@trevor.id}"
 
-      visit "/applications/#{trevor.id}"
       fill_in "Search pets", with: "Rico"
       click_on("search")
-      expect(page).to have_content(rico.name)
+
+      expect(page).to have_content(@rico.name)
       expect(page).to have_content(ricoboy.name)
     end
 
     it 'can find a pet without being case sensitive' do
-      ddfl = Shelter.create!(
-        name: "Denver Dumb Friends League",
-        address: "123 Doggie Lane",
-        city: "Denver",
-        state: "CO",
-        zip: 80246
-      )
-      rico = ddfl.pets.create!(
-        name: "Rico",
-        approximate_age: 4,
-        description: "Staffordshire Terrier",
-        sex: "male"
-      )
-      ricoboy = ddfl.pets.create!(
+      ricoboy = @ddfl.pets.create!(
         name: "Ricoboy",
         approximate_age: 4,
         description: "Staffordshire Terrier",
         sex: "male"
       )
-      trevor = Application.create!(
-        name: "Trevor Suter",
-        street_address: "1275 Birch Lane",
-        city: "Denver",
-        state: "CO",
-        zip: 80220
-      )
+      visit "/applications/#{@trevor.id}"
 
-      visit "/applications/#{trevor.id}"
       fill_in "Search pets", with: "RiCo"
       click_on("search")
-      expect(page).to have_content(rico.name)
+
+      expect(page).to have_content(@rico.name)
       expect(page).to have_content(ricoboy.name)
     end
   end

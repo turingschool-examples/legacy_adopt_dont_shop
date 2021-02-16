fafbf8b6d5718eef21f968631e7c0e3fcadc223b
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-02-16 16:16:02 -0700

    Finish migration to have default for user status

--- db/migrate/20210216230705_change_status_from_user_applications.rb
+++
@@ -1,5 +0,0 @@
-class ChangeStatusFromUserApplications < ActiveRecord::Migration[5.2]
-  def change
-    change_column :user_applications, :status, default: "In Progress"
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_225448) do
+ActiveRecord::Schema.define(version: 2021_02_16_231319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2021_02_16_225448) do
     t.string "state"
     t.string "zip"
     t.string "description"
-    t.string "status"
+    t.string "status", default: "In Progress"
   end
 
   add_foreign_key "pet_applications", "pets"

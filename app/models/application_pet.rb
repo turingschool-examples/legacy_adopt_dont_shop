3a180d452f8384e2d8027de0b6aef067441c1eb5
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-16 10:25:58 -0500

    Complete application status column, complete status approve and reject methods

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -9,14 +9,18 @@ class ApplicationPet < ApplicationRecord
     self.application_status = "Pending"
   end
 
-  def self.approve(app_pet_id, app_application_id)
-    # binding.pry
-    find_by(pet_id: app_pet_id, application_id: app_application_id)
+  def self.status(app_pet_id, app_application_id)
+    app_pet = find_by(pet_id: app_pet_id, application_id: app_application_id)
+    app_pet.application_status
   end
 
-  def self.status(app_pet_id, app_application_id)
-    # binding.pry
-    result = find_by(pet_id: app_pet_id, application_id: app_application_id)
-    result.application_status
+  def self.approve(app_pet_id, app_application_id)
+    app_pet = find_by(pet_id: app_pet_id, application_id: app_application_id)
+    app_pet.update(application_status: "Approved")
+  end
+
+  def self.reject(app_pet_id, app_application_id)
+    app_pet = find_by(pet_id: app_pet_id, application_id: app_application_id)
+    app_pet.update(application_status: "Rejected")
   end
 end

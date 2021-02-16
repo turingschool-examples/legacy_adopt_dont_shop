a37bfdffe26caab7a618d978278c3e0eb7fead5d
Dominic Padula <domopadula2192@gmail.com>
2021-02-16 07:22:54 -0700

    add if statement to change pet and app params when approve application is clicked

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -2,4 +2,16 @@ class AdminApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def status
+    if params[:approve]
+    @application = Application.find(params[:id])
+    @pet = Pet.find(params[:approve])
+    @pet.update({adoptable: false})
+    @application.update({application_status: "approved"})
+    redirect_to "/admin/applications/#{@application.id}?approve=#{@pet.id}"
+    elsif params[:reject]
+      redirect_to "/admin/applications/#{@application.id}?approve=#{@pet.id}"
+    end
+  end
 end

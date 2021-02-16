e85a2c74656298735d41ef7a5a90014c70ae5f36
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-16 09:58:44 -0500

    Add approve and status method starters

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -1,4 +1,15 @@
 class ApplicationPet < ApplicationRecord
   belongs_to :application
   belongs_to :pet
+
+  def self.approve(app_pet_id, app_application_id)
+    binding.pry
+    find_by(pet_id: app_pet_id, application_id: app_application_id)
+  end
+
+  def self.status(app_pet_id, app_application_id)
+    binding.pry
+    result = find_by(pet_id: app_pet_id, application_id: app_application_id)
+    result.application_status
+  end
 end

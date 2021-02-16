eaca3ebf02f685015c375453cddcc748e3ef85cb
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-16 10:20:16 -0500

    Clean up test to read more clearly

--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -9,7 +9,6 @@ describe ApplicationPet, type: :model do
   describe "validations" do
     it { should validate_presence_of :pet_id}
     it { should validate_presence_of :application_id}
-    it { should validate_presence_of :application_status}
   end
 
   describe "class methods" do
@@ -18,12 +17,27 @@ describe ApplicationPet, type: :model do
         application = create(:application, status: "Pending")
         pet1 = create(:pet)
         pet2 = create(:pet)
+        binding.pry
         application.pets << pet1
         application.pets << pet2
 
         ApplicationPet.approve(pet1.id, application.id)
 
         expect(ApplicationPet.status(pet1.id, application.id)).to eq ("Approved")
+        expect(ApplicationPet.status(pet2.id, application.id)).to eq ("Pending")
+      end
+
+      it "should be able to reject pets on applicaitons" do
+        application = create(:application, status: "Pending")
+        pet1 = create(:pet)
+        pet2 = create(:pet)
+        application.pets << pet1
+        application.pets << pet2
+
+        ApplicationPet.reject(pet1.id, application.id)
+
+        expect(ApplicationPet.status(pet1.id, application.id)).to eq ("Rejected")
+        expect(ApplicationPet.status(pet2.id, application.id)).to eq ("Pending")
       end
     end
   end

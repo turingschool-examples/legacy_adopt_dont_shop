5a37baf95774f84b0e43ef37c503a1265f93c6e8
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-15 20:46:28 -0500

    Add tests for admin routes

---
+++ spec/features/admin/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "admin app index page" do
+  before :each do
+    @shelter = create(:shelter)
+    @application_1 = create(:application)
+    @application_2 = create(:application)
+    @application_3 = create(:application)
+    @application_4 = create(:application)
+    @application_5 = create(:application)
+  end
+
+  it "should show a list of the applicants" do
+
+    visit "/applications/"
+
+    expect(page).to have_content(@application_1.first_name)
+    expect(page).to have_content(@application_2.first_name)
+    expect(page).to have_content(@application_3.first_name)
+    expect(page).to have_content(@application_4.first_name)
+    expect(page).to have_content(@application_5.first_name)
+  end
+end

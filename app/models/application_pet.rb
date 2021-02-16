3521dc225916fd510cf3d1f0eac75b8ee6e39e11
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-16 10:20:56 -0500

    Add callback to assign app_status to Pending anytime a pet is added to an application

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -2,13 +2,20 @@ class ApplicationPet < ApplicationRecord
   belongs_to :application
   belongs_to :pet
 
+  validates :pet_id, presence:true
+  validates :application_id, presence:true
+
+  before_create do
+    self.application_status = "Pending"
+  end
+
   def self.approve(app_pet_id, app_application_id)
-    binding.pry
+    # binding.pry
     find_by(pet_id: app_pet_id, application_id: app_application_id)
   end
 
   def self.status(app_pet_id, app_application_id)
-    binding.pry
+    # binding.pry
     result = find_by(pet_id: app_pet_id, application_id: app_application_id)
     result.application_status
   end

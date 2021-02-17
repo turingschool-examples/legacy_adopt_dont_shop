6b2ad94daa3d71af2e85dec19951f52e874179bf
Dominic Padula <domopadula2192@gmail.com>
2021-02-16 17:01:35 -0700

    tests for user story 12

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Pet.destroy_all
 Application.destroy_all
 Shelter.destroy_all
-ApplicationPet.destroy_all 
-Pet.destroy_all
+ApplicationPet.destroy_all
+
 dominic = Application.create!(name: "Dominic", street: "2298 West 28th Ave", city: "Denver", state: "Colorado", zip_code: 80211, description:"Own 10 cats", application_status: "Pending" )
 jordan = Application.create!(name: "Jordan", street: "2300 West 28th Ave", city: "Denver", state: "Colorado", zip_code: 80211, description:"Own 10 dogs", application_status: "Approved" )
 shelter1 = Shelter.create!(name: "Shady Shelter", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
@@ -17,6 +17,8 @@ shelter2 = Shelter.create!(name: "Shady Shelter", address: "123 Shady Ave", city
 pet1 = shelter1.pets.create!(image:"", name: "Thor", description: "dog", approximate_age: 2, sex: "male")
 pet2 = shelter2.pets.create!(image:"", name: "Athena", description: "cat", approximate_age: 3, sex: "female")
 pet3 = shelter2.pets.create!(image:"", name: "Athena", description: "cat", approximate_age: 3, sex: "female")
+pet4 = shelter2.pets.create!(image:"", name: "Johnny", description: "cat", approximate_age: 3, sex: "female")
+pet5 = shelter2.pets.create!(image:"", name: "Elvis", description: "cat", approximate_age: 3, sex: "female")
 ApplicationPet.create!(application: dominic, pet: pet1)
 ApplicationPet.create!(application: dominic, pet: pet2)
 ApplicationPet.create!(application: dominic, pet: pet3)

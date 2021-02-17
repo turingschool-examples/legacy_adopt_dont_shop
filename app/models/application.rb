59856c108a50b95376c2799c85a33325cfd9af0b
Wil McCauley <wmccauley724@gmail.com>
2021-02-16 18:59:31 -0700

    Add validation for description on update

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,6 +2,7 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
   validates :name, :address, :city, :state, :zip, presence: true
+  validates :description, presence: true, on: :update
 
   enum status: ['In Progress', 'Pending', 'Accepted', 'Rejected']
 end

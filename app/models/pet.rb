73a2898009d35d60cd55683caf83b6bd184a9ae9
Katy La Tour <klatour324@gmail.com>
2021-02-16 21:42:07 -0600

    Add class methods for partial name searches, case insensitivity and updating adoption status

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,10 +4,24 @@ class Pet < ApplicationRecord
   has_many :applications, through: :pet_applications
 
   validates_presence_of :name, :description, :approximate_age, :sex
-
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :approximate_age, numericality: {
               greater_than_or_equal_to: 0
             }
 
   enum sex: [:female, :male]
+
+  def self.pet_name(input)
+    where("name LIKE?", "%#{input[:pet_name]}%")
+  end
+
+  def self.find_by_name(name)
+    where("lower(name) like ?", "%#{name.downcase}%")
+  end
+
+  def update_status
+    if adoptable == true
+      self.update(adoptable: false)
+    end
+  end
 end

618ec794dbdb3167d5890892e609d0d5c04a0f20
Arika Turing <ablenker@gmail.com>
2021-02-17 12:59:26 -0700

    Fix zip validation in application

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -2,4 +2,11 @@ class AdminApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def update
+    @application = Application.find(params[:id])
+    @application.update({
+      status: "Approved"})
+    redirect_to "/admin/applications/#{@application.id}"
+  end
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -20,7 +20,6 @@ describe Application, type: :model do
     it { should validate_length_of(:state).is_at_least(2)}
     it { should validate_length_of(:state).is_at_most(2)}
     it { should validate_presence_of(:zip)}
-    it { should validate_length_of(:zip).is_at_least(5)}
-    it { should validate_length_of(:zip).is_at_most(5)}
+    it { should validate_numericality_of(:zip)}
   end
 end

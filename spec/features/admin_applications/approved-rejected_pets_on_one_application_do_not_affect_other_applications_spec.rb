8ef78b78512a1cb374e53d949d86ca2478f641c2
kylejschulz <kylschulz@gmail.com>
2021-02-17 14:37:32 -0500

    approving one pet does not affect another pet

--- spec/features/admin_applications/approved-rejected_pets_on_one_application_do_not_affect_other_applications_spec.rb
+++ spec/features/admin_applications/approved-rejected_pets_on_one_application_do_not_affect_other_applications_spec.rb
@@ -6,14 +6,16 @@ RSpec.describe 'As a visitor' do
       @application_1 = build(:application)
       @application_1.save
       @application_2 = create(:application)
-      @pet_1 = create(:pet, name: 'Fluffy')
-      @pet_2 = create(:pet, name: 'Fluff')
+      @pet_1 = create(:pet, name: 'Keno')
+      @pet_2 = create(:pet, name: 'Fido')
       @pet_3 = create(:pet, name: 'MR. Fluff')
       @pet_4 = create(:pet, name: 'Flufftastic')
       @application_1.pets << @pet_1
       @application_1.pets << @pet_2
       @application_1.pets << @pet_3
-      @application_1.pets << @pet_4
+      @application_2.pets << @pet_1
+      @application_2.pets << @pet_2
+      @application_2.pets << @pet_3
     end
 
     describe "When there are two applications in the system for the same pet" do
@@ -28,43 +30,85 @@ RSpec.describe 'As a visitor' do
         expect(page).to have_content("#{@pet_1.name.capitalize}")
         expect(page).to have_button("Approve")
         expect(page).to have_button("Reject")
+        click_button("Approve")
         end
+        visit "/admin/applications/#{@application_2.id}"
 
-        within("#application_pet-#{@application_1.application_pets.last.id}") do
-        expect(page).to have_content("#{@pet_4.name.capitalize}")
+        within("#application_pet-#{@application_2.application_pets.first.id}") do
+        expect(page).to have_content("#{@pet_1.name.capitalize}")
         expect(page).to have_button("Approve")
         expect(page).to have_button("Reject")
+        expect(page).to have_no_content("rejected")
+        expect(page).to have_no_content("approved")
+        end
+      end
+
+      it "And I approve or reject the pet for that application" do
+        visit "/admin/applications/#{@application_1.id}"
+
+        within("#application_pet-#{@application_1.application_pets.last.id}") do
+        expect(page).to have_content("#{@pet_3.name.capitalize}")
+        expect(page).to have_button("Approve")
+        expect(page).to have_button("Reject")
+        click_button("Approve")
+        end
+        visit "/admin/applications/#{@application_2.id}"
+
+        within("#application_pet-#{@application_2.application_pets.last.id}") do
+        expect(page).to have_content("#{@pet_3.name.capitalize}")
+        expect(page).to have_button("Approve")
+        expect(page).to have_button("Reject")
+        expect(page).to have_no_content("rejected")
+        expect(page).to have_no_content("approved")
+        end
+      end
+      it "And I approve or reject the pet for that application" do
+        visit "/admin/applications/#{@application_1.id}"
+
+        within("#application_pet-#{@application_1.application_pets.first.id}") do
+        expect(page).to have_content("#{@pet_1.name.capitalize}")
+        expect(page).to have_button("Approve")
+        expect(page).to have_button("Reject")
+        click_button("Reject")
+        end
+        visit "/admin/applications/#{@application_2.id}"
+
+        within("#application_pet-#{@application_2.application_pets.first.id}") do
+        expect(page).to have_content("#{@pet_1.name.capitalize}")
+        expect(page).to have_button("Approve")
+        expect(page).to have_button("Reject")
+        expect(page).to have_no_content("rejected")
+        expect(page).to have_no_content("approved")
+        end
+      end
+
+      it "And I approve or reject the pet for that application" do
+        visit "/admin/applications/#{@application_1.id}"
+
+        within("#application_pet-#{@application_1.application_pets.last.id}") do
+        expect(page).to have_content("#{@pet_3.name.capitalize}")
+        expect(page).to have_button("Approve")
+        expect(page).to have_button("Reject")
+        click_button("Reject")
+        end
+        visit "/admin/applications/#{@application_2.id}"
+
+        within("#application_pet-#{@application_2.application_pets.last.id}") do
+        expect(page).to have_content("#{@pet_3.name.capitalize}")
+        expect(page).to have_button("Approve")
+        expect(page).to have_button("Reject")
+        expect(page).to have_no_content("rejected")
+        expect(page).to have_no_content("approved")
         end
       end
 
       it "When I visit the other application's admin show page" do
         visit "/admin/applications/#{@application_1.id}"
-
-        within("#application_pet-#{@application_1.application_pets.first.id}") do
-          expect(page).to have_content("#{@pet_1.name.capitalize}")
-          expect(page).to have_button("Approve")
-          expect(page).to have_button("Reject")
-          click_button("Reject")
-          expect(current_path).to eq("/admin/applications/#{@application_1.id}")
-          expect(page).to have_content("rejected")
-          expect(page).to have_no_button("Reject")
-          expect(page).to have_no_button("Approve")
-        end
-        save_and_open_page
       end
 
       it "Then I do not see that the pet has been accepted or rejected for that application" do
         visit "/admin/applications/#{@application_1.id}"
-        within("#application_pet-#{@application_1.application_pets.last.id}") do
-          expect(page).to have_content("#{@pet_4.name.capitalize}")
-          expect(page).to have_button("Approve")
-          expect(page).to have_button("Reject")
-          click_button("Reject")
-          expect(current_path).to eq("/admin/applications/#{@application_1.id}")
-          expect(page).to have_content("rejected")
-          expect(page).to have_no_button("Approve")
-          expect(page).to have_no_button("Reject")
-        end
+
       end
 
       it "And instead I see buttons to approve or reject the pet for this specific application" do

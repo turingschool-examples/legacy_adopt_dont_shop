01ef10bace3aabc13a2748675f689a558ea22fda
Jacob Volpe <volpe.jacob@gmail.com>
2021-02-16 21:12:54 -0700

    Finish user story 15, still no tests, real bad habit

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -7,6 +7,7 @@ class AdminsController < ApplicationController
     @application = Application.find(params[:id])
     @application.change_pet_status(params[:commit], params[:pet_id], params[:id])
     @application.change_status(params[:id])
+    Pet.change_adopt_status(params[:id])
     redirect_to "/admin/applications/#{params[:id]}"
   end
 end

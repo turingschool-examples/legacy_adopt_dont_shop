7ab03c96f5c23769207731083e16cc04e63c7f75
Katy La Tour <klatour324@gmail.com>
2021-02-16 23:48:16 -0600

    Add validations and raw sql method for ordering in reverse alphabetical

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,9 @@
 class Shelter < ApplicationRecord
   has_many :pets
+  validates_presence_of :name
+  validates_uniqueness_of :name
+
+  def self.order_in_reverse_alphabetical
+    find_by_sql "select * from shelters order by name desc;"
+  end
 end

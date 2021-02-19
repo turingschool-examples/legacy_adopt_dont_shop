b679a309579f4044372f7d8067aa7b0f722f82bf
Wil McCauley <wmccauley724@gmail.com>
2021-02-18 17:52:35 -0700

    Add update method with redirect

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -6,4 +6,15 @@ class PetApplicationsController < ApplicationController
 
     redirect_to "/applications/#{params[:app_id]}"
   end
+
+  def update
+    pet_app = PetApplication.where(pet_id: params[:pet_id], application_id: params[:app_id]).first
+    if params[:update_type] == 'approve'
+      pet_app.update(status: 1)
+    elsif params[:update_type] == 'reject'
+      pet_app.update(status: 2)
+    end
+
+    redirect_to "/admin/applications/#{params[:app_id]}"
+  end
 end

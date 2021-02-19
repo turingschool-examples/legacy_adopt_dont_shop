6cb7ed4946cf64a579c192b5e790e7b911df21a5
Wil McCauley <wmccauley724@gmail.com>
2021-02-18 17:52:08 -0700

    Add pet_app_hash instance variable to pass for element hiding

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,8 @@
 class AdminsController < ApplicationController
   def show
     @application = Application.find(params[:id])
+    @pet_app_hash = @application.pets.each_with_object({}) do |pet, hash|
+      hash[pet.name] = PetApplication.find_by(pet_id: pet.id, application_id: @application.id)
+    end
   end
 end
